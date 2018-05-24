0a6ca3ece42a9d2c2158d389b8370dd5ee23730e
Nightmare2112 <rbrooks@FILAUSA.com>
2018-05-24 07:14:43 -0400

    Added Invite Link

--- Commands/help.js
+++ Commands/help.js
@@ -7,6 +7,7 @@ exports.run = (client, message, args) => {
         .addField("__😄 Avatar 😄__ - **&Avatar <mention>**","Description: Using will display avatar of mentioned member.",false)
         .addField("__⚔ Stats ⚔__ - **&Stats <mention> or ID**","Description: Using will display DRPG detailed stats for the user or ID mentioned",false)
         .addField("__🎥 Actions 🎥__ - &<action> <mention>","Description:Using the below `actions` with ping will display gif with mentioned member.\n**Actions:** Adorbs, Bite, Cuddle, Hug, Kidnap, Kiss, Lick, Tackle,\nSlap, Spank",false)
+        .addField("__Add Me To Your Server__","You can add me to your server with the link below\nhttps://discordapp.com/oauth2/authorize?client_id=437802197539880970&scope=bot&permissions=8",false)
         .setFooter("In Development By Nightmare#1234")
         .setTimestamp ()
             message.channel.send({embed});
