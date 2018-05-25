34a95e5e63b75a54a29e3b7dac0c524d2094944b
Nightmare2112 <rbrooks@FILAUSA.com>
2018-05-25 01:52:28 -0400

    update

--- events/message.js
+++ events/message.js
@@ -30,7 +30,7 @@ exports.run = (client, message) => {
       if (test[3] != `+ Critical hit!`) {
         healthraw = test[8]
       };
-      var editedhealth = healthraw.split(` `)[3]
+      var editedhealth = (healthraw != undefined) ? healthraw.split(` `)[3] : 0
       message.channel.send(`Current Health ${editedhealth}`)
       
       ///if (test[0].startsWith(`!`)){
