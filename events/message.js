a30ceb17054f2b48effde471f4785f4e2f38bfb2
Nightmare2112 <rbrooks@FILAUSA.com>
2018-05-25 02:43:53 -0400

    Added Pollux Box Watch

--- events/message.js
+++ events/message.js
@@ -1,9 +1,9 @@
 exports.run = (client, message) => {
   //Pollux Lootbox Watcher
-    if (message.author.id == `` ){
+    if (message.author.id == `271394014358405121` ){
       console.log(message.content)
       if(message.content.endsWith(`a chance to claim it!`)) {
-        message.channel.send(`Come Grab My Box <@310296184436817930> <@384996823556685824> <@266632489546678273>`)
+        message.channel.send(`Come Grab My Box <@310296184436817930> <@384996823556685824> <@266632489546678273> <@207575115616092161>`)
       }
 
     };
