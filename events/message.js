db6378b1e15ba7f737bcf2edc517b8fef6033bb3
Nightmare2112 <rbrooks@FILAUSA.com>
2018-05-29 00:06:49 -0400

    update too user list

--- events/message.js
+++ events/message.js
@@ -3,7 +3,7 @@ exports.run = async (client, message) => {
   if (message.author.id == `271394014358405121`){
     console.log(message.content)
     if(message.content.endsWith(`a chance to claim it!`) && message.author.id == "271394014358405121") {
-      message.channel.send(`Come Grab My Box <@310296184436817930> <@384996823556685824> <@266632489546678273> <@207575115616092161> <@226358525410934794>`)
+      message.channel.send(`Come Grab My Box <@310296184436817930> <@384996823556685824> <@266632489546678273> <@207575115616092161> <@226358525410934794> <@267906052366794753> <@348317596606529536> <@196203663054733313>\n<@&440727495906689024>`)
     }
   } else if (message.author.id == `170915625722576896`) {
     const Discord = require("discord.js");
