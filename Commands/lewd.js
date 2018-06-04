cce2b04a43e95319218af48475cf6a81ce0b6639
Nightmare2112 <rbrooks@FILAUSA.com>
2018-06-04 03:00:41 -0400

    Intial lewd.js commit

---
+++ Commands/lewd.js
@@ -0,0 +1,27 @@
+exports.run = (client, message, args) => {
+    const lewds = require("./../Data/imageurls.json");
+    var sendlewds = (`${lewds.lewds[~~(Math.random() * lewds.lewds.length)]}`);
+    if(message.channel.nsfw === true){
+        if(message.mentions.users.first()) { //Check if the message has a mention in it.
+            let target = message.mentions.users.first();
+            message.channel.send({embed:{
+                author:{
+                    name: message.author.username,
+                    icon_url: message.author.avatarURL
+                },
+                description: (message.author +` is being lewd towards `+ target),
+                image: {
+                    url : (sendlewds),
+                },
+                timestamp: new Date()
+        
+            }});
+        }else {
+            message.reply("Please mention someone :thinking:"); //Reply with a mention saying "Invalid user."
+        };
+
+    }else{
+        message.reply("Bad bad bad!! - You can only use this command in a channel marked *NSFW*")
+    }
+
+};
