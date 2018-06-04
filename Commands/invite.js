b97697fa50fb18642021e2c3b307863bdb7f255d
Nightmare2112 <rbrooks@FILAUSA.com>
2018-06-04 00:28:47 -0400

    Intial Commit

--- Commands/invite.js
+++ Commands/invite.js
@@ -2,8 +2,7 @@ exports.run = (client, message, args) => {
     const Discord = require("discord.js");
     const embed = new Discord.RichEmbed()
         .setTitle("Add Me To Your Server")
-        .setAuthor("Author")
-        .addField("__Link__","https://discordapp.com/oauth2/authorize?client_id=437802197539880970&scope=bot&permissions=8",false)
+        .addField("__Link__","http://bit.do/aldebaranbot",false)
         .setFooter("In Development By Nightmare#1234")
         .setTimestamp ()
     message.channel.send({embed});
