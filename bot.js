7a0f15f4e5e4b1a4fea5fc3983b21b74a388bddf
Brooks <rbrooks@FILAUSA.com>
2018-06-05 05:42:58 -0400

    "Adding DB"

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs");
+const mysql = require("mysql");
 
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
