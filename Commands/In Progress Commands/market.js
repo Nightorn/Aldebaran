edc289a84c10cf7143816a921782742c520bb1b1
Brooks <rbrooks@FILAUSA.com>
2018-06-08 13:37:10 -0400

    update to questinfo

--- Commands/In Progress Commands/market.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
-    const Discord = require("discord.js");
-    const apikey = require("./../config.json");
-    const request = require('request');
-    const itemlist = require('./../Data/drpgitemlist.json')
-
-}
-    
