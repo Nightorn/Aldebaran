53f06b6e520cb3f23045b58eed80b948ff207378
Brooks <rbrooks@FILAUSA.com>
2018-06-11 02:18:46 -0400

    added cat & xp commands, updates to config and package.json

---
+++ Commands/cat.js
@@ -0,0 +1,22 @@
+exports.run = (client, message, args) => {
+    const request = require(`request`)
+    const Discord = require(`discord.js`)
+    const config = require(`./../config.json`)
+    const parser = require("xml2js")
+
+    request({uri: `http://thecatapi.com/api/images/get?api_key=${config.cat_apikey}&format=xml&results_per_page=1`}, function (err, response, body) {
+        if (err) return message.channel.send("The seems to be a birb problem")
+        var data = (body)
+        parser.parseString(data, function (err, result) {
+            var image = result.response.data[0].images[0].image[0].url[0]
+            var imagesource = result.response.data[0].images[0].image[0].source_url[0]
+            const embed = new Discord.RichEmbed()
+            .setTitle(`**__Here kitty kitty!__**`)
+            .setAuthor(message.author.username,message.author.avatarURL)
+            .setColor(0x00AE86)
+            .setImage(`${image}`)
+            .setFooter(`Cat Powered By: ${imagesource}`)
+            message.channel.send({embed})
+        })
+    })
+}
---
+++ Commands/xp.js
@@ -0,0 +1,25 @@
+exports.run = (client, message, args) => {
+    const Discord = require("discord.js");
+    var level = args[0];
+    if (level >= 1){
+        var formula1 = Math.floor((1249297 * (level * level)) /61200000);
+        var formula2 = Math.floor((1778779 * level) / 306000);
+        var formula3 = Math.floor(11291 / 51);
+        var finalnoring = Math.floor((formula1 + formula2 + formula3) / 1.5);
+        var finalxpring = Math.floor(((formula1 + formula2 + formula3) / 1.5) * 1.25);
+        var finaldonorring = Math.floor((formula1 + formula2 + formula3));
+        
+        const embed = new Discord.RichEmbed()
+            .setTitle(`Average Xp Kill At Lvl. ${level}`)
+            .setAuthor(message.author.username,message.author.avatarURL)
+            .setColor(0x00AE86)
+            .setDescription(`**Please note all infomation about xp are strictly estimates!!**\n*Estimates are based on FULL xpboost build, while grinding Dynomobs.*`)
+            .addField(`**__With Ring of XP (1.25X)__**`,` Estimated ${finalxpring}xp Per Kill`,true)
+            .addField(`**__With Donor XP Ring (1.5X)__**`,` Estimated ${finaldonorring}xp Per Kill`,true)
+            .addField(`**__Without XP Ring__**`,` Estimated ${finalnoring}xp Per Kill`,false)
+        
+        message.channel.send({embed})
+    
+    }else message.reply(`You must specify a level.`)
+
+}
