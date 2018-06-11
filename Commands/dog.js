c0cd5da8a3f0c97cd5d72800375e5c7cd35e22b0
Brooks <rbrooks@FILAUSA.com>
2018-06-11 04:31:23 -0400

    initial commit

---
+++ Commands/dog.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+    const request = require(`request`)
+    const Discord = require(`discord.js`)
+    const config = require(`./../config.json`)
+    request({uri: `https://random.dog/woof.json`} , function (err, response, body) {
+        if (err) return message.channel.send("There seems to be a dogo problem")
+        const data = JSON.parse(body)
+        var image = data.url
+        const embed = new Discord.RichEmbed()
+        
+        .setTitle(`**__Woof Woof__**`)
+        .setAuthor(message.author.username,message.author.avatarURL)
+        .setColor(0x00AE86)
+        .setImage(`${image}`)
+        .setFooter(`Dogo Powered By: http://random.dog`)
+        message.channel.send({embed})
+
+    });
+}
