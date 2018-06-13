746f0e7d570d4f0f3711d872e18a4c2303eb8549
Brooks <rbrooks@FILAUSA.com>
2018-06-13 02:24:19 -0400

    intial commit

---
+++ Commands/ActionCommands/pat.js
@@ -0,0 +1,31 @@
+exports.run = (bot, message, args) => {
+    const client = require('nekos.life');
+    const Discord = require(`discord.js`)
+    const neko = new client();
+    if(message.mentions.users.first()) { //Check if the message has a mention in it.
+        let target = message.mentions.users.first();
+        async function pat() {
+            const data = (await neko.getSFWPat());
+            message.channel.send({embed:{
+                author:{
+                    name: message.author.username,
+                    icon_url: message.author.avatarURL
+                },
+                description: (message.author +`  loves patting `+ target),
+                image: {
+                    url : (data.url),
+                },
+                timestamp: new Date(),
+                footer: {
+                    icon_url: bot.avatarURL,
+                    text: "Powerd By Nekos.life"
+                }
+            
+            }});
+        }
+        pat();
+        
+    } else {
+        message.reply("Please mention someone :thinking:")
+    }
+}
