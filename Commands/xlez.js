c404d9f102f0029f78607f0a046fa6643237f30b
Brooks <rbrooks@FILAUSA.com>
2018-06-12 21:47:17 -0400

    inital commits

---
+++ Commands/xlez.js
@@ -0,0 +1,31 @@
+exports.run = (bot, message, args) => {
+    const client = require('nekos.life');
+    const Discord = require(`discord.js`)
+    const neko = new client();
+    if(message.channel.nsfw === true) { //Check if the message has a mention in it.
+        let target = message.mentions.users.first();
+        async function lez() {
+            const data = (await neko.getNSFWLesbian());
+            message.channel.send({embed:{
+                author:{
+                    name: message.author.username,
+                    icon_url: message.author.avatarURL
+                },
+                description: (message.author + " " + ` LEZ be Honest!`),
+                image: {
+                    url : (data.url),
+                },
+                timestamp: new Date(),
+                footer: {
+                    icon_url: bot.avatarURL,
+                    text: "Powerd By Nekos.life"
+                }
+            
+            }});
+        }
+        lez();
+        
+    } else {
+        message.reply("Tsk tsk! This command is only usable in a NSFW channel.")
+    }
+}
