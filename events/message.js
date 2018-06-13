8aee7b1a5f5021ab0f31b9c068c7ae10a7272583
Brooks <rbrooks@FILAUSA.com>
2018-06-13 07:42:01 -0400

    updates

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
 exports.run = async (client, message) => {
-  if (message.author.id == `271394014358405121`) require('./../functions/bots/Pollux')(client, message);
-  else if (message.author.id == `170915625722576896`) require('./../functions/bots/DiscordRPG')(client, message);
+  if (message.author.id == `271394014358405121`) require('./../functions/bots/Pollux.js')(client, message);
+  else if (message.author.id == `170915625722576896`) require('./../functions/bots/DiscordRPG.js')(client, message);
 }   
