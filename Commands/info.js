806966de6f63d4c884b21bb947ec128322e8ee45
Brooks <rbrooks@FILAUSA.com>
2018-06-17 14:06:13 -0400

    intial commit

---
+++ Commands/info.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+exports.run = function(bot, message, args) {
+    const minutesUptime = `${String(Math.floor(bot.uptime%3600000/60000))}m`;
+    const embed = new Discord.RichEmbed()
+        .setAuthor(bot.user.username, bot.user.avatarURL)
+        .setTitle('Details About Aldebaran Bot')
+        .addField(`__Developers__`, `<@310296184436817930>\n<@320933389513523220>`, false)
+        .addField(`__Statistics__`, `**Servers** : ${bot.guilds.size}\n**Channels** : ${bot.channels.size}\n**Users** : ${bot.users.size}`, true)
+        .addField(`__Resources__`, `**Memory Usage** : ${Math.round(100 * process.memoryUsage().heapTotal/1000000) / 100} MB\n**Uptime** : ${Math.floor(bot.uptime/3600000)}h${minutesUptime.length == 2 ? '0' + minutesUptime : minutesUptime}`, true)
+        .addField(`__Host__`, `Heroku`, false)
+        .addField(`__Prefix__`, `**&**`, false)
+        .setThumbnail(message.guild.iconURL)
+        .setColor(`GREEN`);
+    message.channel.send({embed: embed});
+}
