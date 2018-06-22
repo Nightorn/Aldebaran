01c032f366986c4aff45d239326cafc81e291b37
Brooks <rbrooks@FILAUSA.com>
2018-06-22 05:17:29 -0400

    update

--- Commands/serverlist.js
+++ Commands/serverlist.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message) => {
-    if(message.author.id != `310296184436817930` || `320933389513523220`);return message.channel.send(`YOU SHALL NOT PASS!!!`)
+    if(message.author.id != `310296184436817930` || message.author.id != `320933389513523220`);return message.channel.send(`YOU SHALL NOT PASS!!!`)
     let string = "";
     client.guilds.forEach(guild =>{
         string += `Guild Name: ${guild.name}\n`
