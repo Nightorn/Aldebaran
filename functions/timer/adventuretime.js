ec19b22c77b00f0dbe927805b80e04b510cc8f67
Brooks <rbrooks@FILAUSA.com>
2018-06-25 01:46:27 -0400

    update

--- functions/timer/adventuretime.js
+++ functions/timer/adventuretime.js
@@ -1,6 +1,6 @@
 const config = require('./../../config.json');
 module.exports = function(bot, message, args) {
-    if (message.content.startsWith(`#!adv`)){
+    if (message.content.toLowerCase().startsWith(`#!adv`)){
       message.delete(1000);
       setTimeout((channel, userid) => {
         message.channel.send(`<@${message.author.id}> adventure time! :crossed_swords:`).then(msg => {
@@ -9,7 +9,7 @@ module.exports = function(bot, message, args) {
       }, 13500, message.channel, message.author.id)
     }
 
-    else if (message.content.startsWith(`,adv`)){
+    else if (message.content.toLowerCase().startsWith(`,adv`)){
       message.delete(1000);
       setTimeout((channel, userid) => {
         message.channel.send(`<@${message.author.id}> adventure time! :crossed_swords:`).then(msg => {
@@ -18,7 +18,7 @@ module.exports = function(bot, message, args) {
       }, 13500, message.channel, message.author.id)  
     }
     
-    else if (message.content.startsWith(`.adv`)){
+    else if (message.content.toLowerCase().startsWith(`.adv`)){
       message.delete(1000);
       setTimeout((channel, userid) => {
         message.channel.send(`<@${message.author.id}> adventure time! :crossed_swords:`).then(msg => {
