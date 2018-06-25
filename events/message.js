5041dcd8091bb1cbe43330856121d1b1ad4408ad
Brooks <rbrooks@FILAUSA.com>
2018-06-25 01:41:00 -0400

    test

--- events/message.js
+++ events/message.js
@@ -2,5 +2,5 @@ exports.run = async (client, message) => {
   if (message.author.id == `271394014358405121`) require('./../functions/bots/Pollux.js')(client, message);
   else if (message.author.id == `170915625722576896`) require('./../functions/bots/DiscordRPG.js')(client, message);
   else if (message.content === 'kek kek') message.delete();
-  else if (message.author.id == `310296184436817930` ) require(`./../functions/timer/adventuretime.js`)(client,message);  
+  else if (message.author.id == `310296184436817930` || message.author.id == `368059724937166850` ) require(`./../functions/timer/adventuretime.js`)(client,message);  
 } 
