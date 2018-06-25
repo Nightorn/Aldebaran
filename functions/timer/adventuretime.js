0fb5e230f93611e9737a9a8f8117a6cef1680639
Brooks <rbrooks@FILAUSA.com>
2018-06-25 01:50:33 -0400

    update

--- functions/timer/adventuretime.js
+++ functions/timer/adventuretime.js
@@ -1,12 +1,14 @@
 const config = require('./../../config.json');
 module.exports = function(bot, message, args) {
     if (message.content.toLowerCase().startsWith(`#!adv`)){
-      message.delete(1000);
-      setTimeout((channel, userid) => {
-        message.channel.send(`<@${message.author.id}> adventure time! :crossed_swords:`).then(msg => {
-          msg.delete(3000)
-        }).catch();
-      }, 13500, message.channel, message.author.id)
+      if(message.channel.id == `172388293999198208`){
+        message.delete(1000);
+        setTimeout((channel, userid) => {
+          message.channel.send(`<@${message.author.id}> adventure time! :crossed_swords:`).then(msg => {
+            msg.delete(3000)
+          }).catch();
+        }, 13500, message.channel, message.author.id)
+      }
     }
 
     else if (message.content.toLowerCase().startsWith(`,adv`)){
