7e3b816a5eaaea1d8d1d612d4ebea7f02a538a19
Brooks <rbrooks@FILAUSA.com>
2018-06-25 06:42:18 -0400

    update to permissions

--- Commands/invite.js
+++ Commands/invite.js
@@ -2,7 +2,7 @@ exports.run = (bot, message, args) => {
     const Discord = require("discord.js");
     const embed = new Discord.RichEmbed()
         .setTitle("Add Me To Your Server")
-        .addField("__Link__","http://bit.do/aldebaranbot",false)
+        .addField("__Link__","http://tiny.cc/aldebaran",false)
         .setFooter("In Development By Nightmare#1234")
         .setTimestamp ()
     message.channel.send({embed});
