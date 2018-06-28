4a729f97962921b16185c815906d4276467eed09
Brooks <rbrooks@FILAUSA.com>
2018-06-28 04:02:07 -0400

    updated invite to include Support server link

--- Commands/invite.js
+++ Commands/invite.js
@@ -1,9 +1,8 @@
 exports.run = (bot, message, args) => {
     const Discord = require("discord.js");
-    const embed = new Discord.RichEmbed()
-        .setTitle("Add Me To Your Server")
-        .addField("__Link__","http://tiny.cc/aldebaran",false)
-        .setFooter("In Development By Nightmare#1234")
-        .setTimestamp ()
-    message.channel.send({embed});
+    message.channel.send({"embed": {
+        "title": "Invite Links",
+        "description": "[__**Add Bot To Your Server**__](https://discordapp.com/api/oauth2/authorize?client_id=437802197539880970&permissions=126016&scope=bot)\n\n[__**Join Support Server**__](https://discord.gg/3x6rXAv)\n*Stay updated with the newest features and commands*", 
+        "footer": {"text": "In Development By Nightmare#1234"}
+      }})
 }
