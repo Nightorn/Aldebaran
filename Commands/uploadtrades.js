ed315b39f5cc986b0ceb29c941d960b76a5d588b
Nightmare#1234 <brooksroy87@gmail.com>
2018-06-29 03:26:46 -0400

    fixed duplicated var

---
+++ Commands/uploadtrades.js
@@ -0,0 +1,24 @@
+const poolQuery = require('./../functions/database/poolQuery');
+const Discord = require("discord.js");
+const request = require(`request`);
+const config = require(`./../config.json`)
+exports.run = function(client, message, args, tradeapilimit, ratelimitrest) {
+    if (ratelimitrest != null || Date.now() < ratelimitrest)return;
+    if (message.author.id != `310296184436817930`)
+    request({uri:`http://api.discorddungeons.me/v3/trade`, headers: {"Authorization":config.drpg_apikey} }, function(err, response, body) {
+        if (err) return;
+        ratelimitrest = Math.floor(Date.now() + (5 * 60 * 1000))
+        const data = JSON.parse(body);
+        for(let [value] of Object.entries(data)){
+            console.log(value.id)
+            tradeid = value.id
+            markettype =value.type
+            totalquant = (value.buy != undefined) ? value.buy.item : value.sell.item;
+            quantcomplete = (value.bought != undefined) ? value.bought : value.sold;
+            price = (value.buy !=undefined) ? value.buy.price : value.sell.price;
+            userid = value.from
+            itemid = (value.buy != undefined) ? value.buy.item : value.sell.item;
+            quantremaining = Math.floor(totalquant - quantcomplete) 
+        }   
+    });
+}
