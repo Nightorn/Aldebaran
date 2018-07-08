bc17d3718d985962661a7d0cb143d8a98c64fe5d
Nightmare#1234 <brooksroy87@gmail.com>
2018-07-08 12:53:05 -0400

    Database Conenctions 10 -> 13

--- functions/database/returnPool.js
+++ functions/database/returnPool.js
@@ -1,7 +1,7 @@
 const config = require('./../../config.json');
 const mysql = require('mysql');
 var pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 13,
     host: config.mysql.host,
     user: config.mysql.user,
     password: config.mysql.password,
