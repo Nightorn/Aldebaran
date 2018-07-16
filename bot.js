edd02ff00047820c139a7aaf811cbaabcabe5f8f
Ciborn <rb.ciborn+dev@gmail.com>
2018-07-16 19:59:52 +0200

    update(event): new command handler with cooldown manager

--- bot.js
+++ bot.js
@@ -1,13 +1,10 @@
+const CooldownManager = require('./structures/CooldownManager');
+const Command = require('./structures/Command');
+const config = require("./config.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client(); 
-const config = require("./config.json");
 const fs = require("fs");
-const mysql = require("mysql");
-var itemapicooldown = false;
-var globalcooldown = false;
-var apiratelimit ;
-var ratelimitreset ;
-var update;
+const cooldownManager = new CooldownManager();
 
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
@@ -17,60 +14,29 @@ fs.readdir("./events/", (err, files) => {
       // super-secret recipe to call events with all their proper arguments *after* the `client` var.
       bot.on(eventName, (...args) => eventFunction.run(bot, ...args));
     });
-  });
+});
  
 bot.on("message", message => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
+
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-//....................Added API 1min Cooldown Single Item Search................................................//    
-    if (command == "mcsearch") {
+    
+    /*if (command == "mcsearch") {
       if (itemapicooldown == true) return message.reply("Please try again in 1min due to ratelimits.");
         itemapicooldown = true
         setTimeout(() => {
         itemapicooldown = false
       }, 60000);
-    } 
-//..............................................................................................................//
-//....................Added Golbal Spam Cooldown................................................//    
-  if (message.content.startsWith("&")) {
-    if (globalcooldown == true) return message.reply("Stop that, spam hurts!!");
-      globalcooldown = true
-      setTimeout((channel, userid) => {
-      globalcooldown = false
-    }, 500, message.channel, message.author.id);
-  }
-  console.log(`User - ${message.author.id} (${message.author.tag}) | Command - ${command}${args.length === 0 ? '' : ` | Args - ${args}`}`);  
-//..............................................................................................................//
-  const execute = (path) => {
-    let file = require(path);
-    if ((file.infos.category === 'NSFW' && message.channel.nsfw) || file.infos.category !== 'NSFW') {
-      file.run(bot, message, args);
-    } else {
-      message.reply("Tsk tsk! This command is only usable in a NSFW channel.");
-    }
-  }
-
-  try {
-    fs.readdir('./Commands', (err, files) => {
-      if (files.indexOf(`${command}.js`) != -1) {
-        execute(`./Commands/${command}`);
-      } else {
-        for (let fileName of files) {
-          if (fs.statSync(`./Commands/${fileName}`).isDirectory()) {
-            fs.readdir(`./Commands/${fileName}`, (err, files) => {
-              if (files.indexOf(`${command}.js`) != -1) {
-                execute(`./Commands/${fileName}/${command}`);
-              }
-            });
-          }
-        }
-      }
-    });
-  } catch (err) {
-    console.error(err);
-  }
+	} */
+  
+  	try {
+      const isGood = cooldownManager.execute(message.author.id, true, new Command(command), bot, message, args);
+      console.log(`User ${message.author.id} (${message.author.tag}) | Command ${command}${args.length === 0 ? '' : ` | Args - ${args}`} | Cooldown : ${isGood ? `GOOD` : `BAD`}`);
+  	} catch(err) {
+    	if (err.message !== 'Unknown Command') console.error(err);
+  	}
 });
  
-  bot.login(config.token)
+bot.login(config.token);
