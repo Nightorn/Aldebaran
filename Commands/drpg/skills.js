fd331c9a145ae222b728b45e1074f0bdaa645b46
Nightmare#1234 <brooksroy87@gmail.com>
2018-07-25 05:03:07 -0400

    inital beta commit

---
+++ Commands/drpg/skills.js
@@ -0,0 +1,30 @@
+exports.run = (bot, message, args, apiratelimit) => {
+    const Discord = require("discord.js");
+    const apikey = require("./../../config.json");
+    const request = require('request');
+    const locationdb = require("./../../Data/drpglocationlist.json");
+    var usrid = message.author.id;
+    if(args.length > 0){
+        usrid = message.mentions.members.size > 0 ? message.mentions.members.first().id : args[0];
+    };
+        if (apiratelimit == 0)return message.channel.send(`This Command is globally ratelimited, please try again in 1min.`);
+        bot.fetchUser(usrid).then((user) => {
+            request({uri:`http://api.discorddungeons.me/v3/user/${usrid}`, headers: {"Authorization":apikey.drpg_apikey} }, function(err, response, body) {
+                if (err) return;
+                const data = JSON.parse(body);
+                if (data.error != undefined)return message.channel.send(`**Error**: No User Profile Found.`)
+                apiratelimit = response.headers["x-ratelimit-remaining"]
+                var ratelimitrest = Math.floor(parseInt(response.headers["x-ratelimit-reset"] - (Date.now()/1000)))
+                const embed = new Discord.RichEmbed()
+                .setTitle(data.name + "'s Skill Info")
+                .setAuthor(message.author.username,message.author.avatarURL)
+                .setColor(0x00AE86)
+                .setFooter(`${apiratelimit} Global Uses Remain Before Ratelimited | Usages Reset In ${ratelimitrest} seconds.`)
+                .addField(`**__Chopping__**`,`**Level:** *${data.skills.chop.level}*\n**Total XP:** ${data.skills.chop.xp}`)
+                message.send(embed)
+            });
+        })
+        .catch(err => {
+            message.reply("Error you must enter a valid UserID or User Mention")
+        })
+}
