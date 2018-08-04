6ceb182c3b4ca9179a778c5fe22c5a1f3b059413
Nightmare2112 <brooksroy87@gmail.com>
2018-08-03 23:40:59 -0400

    revert testing O.o

--- functions/timer/padv.js
+++ functions/timer/padv.js
@@ -3,12 +3,12 @@ module.exports = (client, message) => {
     if (message.content.toLowerCase().startsWith(`#!padv`)|| message.content.toLowerCase().startsWith(`,padv`)|| message.content.toLowerCase().startsWith(`.padv`)){
         poolQuery(`SELECT settings FROM guilds WHERE guildId='${message.guild.id}'`).then(result => {
             if (Object.keys(result).length != 0) {
-                let settingsg = JSON.parse(result[0].settings);
-                if (settingsg.adventureTimer !== 'off') {
+                let settings = JSON.parse(result[0].settings);
+                if (settings.adventureTimer === 'on') {
                     poolQuery(`SELECT settings FROM users WHERE userId='${message.author.id}'`).then(result => {
                         if (Object.keys(result).length != 0) {
                             let settings = JSON.parse(result[0].settings);
-                            if (settings.adventureTimer !== 'off') {
+                            if (settings.adventureTimer === 'on') {
                                 message.delete(1000);
                                 setTimeout((channel, userid) => {
                                     message.channel.send("<@" + message.author.id + "> Party Time! :tada:");
