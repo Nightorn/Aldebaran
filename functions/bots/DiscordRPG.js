6f93a7fb345d4816d772c5269356cb6becea787e
Ciborn <rb.ciborn+dev@gmail.com>
2018-08-06 03:49:15 +0200

    update: DiscordRPG Warnings and Fixes

--- functions/bots/DiscordRPG.js
+++ functions/bots/DiscordRPG.js
@@ -1,110 +1,106 @@
 const poolQuery = require('./../../functions/database/poolQuery');
 const Discord = require("discord.js");
 module.exports = function(client, message, args) {
-  poolQuery(`SELECT * FROM guilds WHERE guildid ='${message.guild.id}'`).then((result) =>{
-	if (Object.keys(result).length != 0){
-		let settingsg = JSON.parse(result[0].settings);
-		if (settingsg.healthMonitor === 'off')return;
+  	poolQuery(`SELECT * FROM guilds WHERE guildid ='${message.guild.id}'`).then((result) => {
+		if (result.length === 0) return;
+		if (JSON.parse(result[0].settings).healthMonitor === 'off') return;
 		
-		var playerName = null;
-		var char_currentHP = 0;
-		var char_maxHP = 0;
-		var pet_currentHP = 0;
-		var pet_maxHP = 0;
+		var player = {
+			name : null,
+			currentHP : 0,
+			maxHP : 0
+		}, pet = {
+			currentHP : 0,
+			maxHP : 0
+		};
 		var deathimage = require(`./../../Data/imageurls.json`)
 		var senddeath = (`${deathimage.deathimage[~~(Math.random() * deathimage.deathimage.length)]}`);
-		if (message.embeds.length === 0){
-		  if (message.content.indexOf("'s Adventure") != -1) {
-			const regex = /has \d+\d+ HP left./
-			var healthMessagePattern = /( has [\d,]+\/[\d,]+ HP left\.)|(used .+? and got [\d,]+?HP\. \([\d,]+\/[\d,]+HP\))|(Health: [\d,]+\/[\d,]+HP\.)/;
-			var healthMessage = message.content.match(healthMessagePattern);
-			if (healthMessage) {
-			  var nums = healthMessage[0].match(/([\d,]+)\/([\d,]+)/);
-			  char_currentHP = Number(nums[1].replace(/,/g,""));
-			  char_maxHP = Number(nums[2].replace(/,/g,""));
-			  playerName = message.content.split(`\n`)[1].replace("'s Adventure ]========!", "").replace("!========[ ", "");
+		if (message.embeds.length === 0) {
+			if (message.content.indexOf("'s Adventure") != -1) {
+				var healthMessagePattern = /( has [\d,]+\/[\d,]+ HP left\.)|(used .+? and got [\d,]+?HP\. \([\d,]+\/[\d,]+HP\))|(Health: [\d,]+\/[\d,]+HP\.)/;
+				var healthMessage = message.content.match(healthMessagePattern);
+				if (healthMessage) {
+					var nums = healthMessage[0].match(/([\d,]+)\/([\d,]+)/);
+					player.currentHP = Number(nums[1].replace(/,/g,""));
+					player.maxHP = Number(nums[2].replace(/,/g,""));
+					player.name = message.content.split(`\n`)[1].replace("'s Adventure ]========!", "").replace("!========[ ", "");
+				}
+				const messageArray = message.content.split('\n');
+				if (messageArray[3] != undefined) {
+					const petInfosLine = messageArray[3].indexOf('+ Critical hit!') != -1 ? messageArray[8].split(' ') : messageArray[7].split(' ');
+					pet.currentHP = parseInt(petInfosLine[petInfosLine.indexOf('has') + 1].split('/')[0].replace(',', ''));
+					pet.maxHP = parseInt(petInfosLine[petInfosLine.indexOf('has') + 1].split('/')[1].replace(',', ''));
+				}
 			}
-	  
-			const messageArray = message.content.split('\n');
-			if (messageArray[3] != undefined) {
-			  const petInfosLine = messageArray[3].indexOf('+ Critical hit!') != -1 ? messageArray[8].split(' ') : messageArray[7].split(' ');
-			  pet_currentHP = parseInt(petInfosLine[petInfosLine.indexOf('has') + 1].split('/')[0].replace(',', ''));
-			  pet_maxHP = parseInt(petInfosLine[petInfosLine.indexOf('has') + 1].split('/')[1].replace(',', ''));
-			}
-		  }
 		}
 		else {
-		  if (message.embeds[0].author != undefined) {
-			if (message.embeds[0].author.name.indexOf("Adventure") != -1) {
-			  const char_field = message.embeds[0].fields[1].name == "Critical Hit!" ? message.embeds[0].fields[4] : message.embeds[0].fields[3];
-			  char_currentHP = parseInt(char_field.value.split(' ')[1].split('/')[0].replace(',', ''));
-			  char_maxHP = parseInt(char_field.value.split(' ')[1].split('/')[1].replace(',', ''));
-	
-			  const pet_field = message.embeds[0].fields[1].name == "Critical Hit!" ? message.embeds[0].fields[3] : message.embeds[0].fields[2];
-			  pet_currentHP = parseInt(pet_field.value.split(' ')[1].split('/')[0].replace(',', ''));
-			  pet_maxHP = parseInt(pet_field.value.split(' ')[1].split('/')[1].replace(',', ''));
-	
-			  playerName = char_field.name;
-			}
-		  }
-		}
-		
-		const user = client.users.find('username', playerName);
-		var char_healthPercent = Math.round(10 * char_currentHP * 100 / char_maxHP) / 10;
-		var pet_healthPercent = Math.round(10 * pet_currentHP * 100 / pet_maxHP) / 10;
-		if (user != null && playerName != undefined && char_healthPercent != pet_healthPercent) {
-		  poolQuery(`SELECT * FROM users WHERE userId='${user.id}'`).then((result) => {
-			const drpg = function() {
-			  if (char_healthPercent < 11) {
-				const embed = new Discord.RichEmbed()
-				  .setTitle(`__${user.username} Health Warning!!! - ${char_healthPercent}%__`)
-				  .setColor(0xff0000)
-				  .setDescription(`**${user.username}** is at __**${char_currentHP}**__ health!!!\n`)
-				  .setImage(`${senddeath}`)
-				  .setFooter(`You are going to die aren't you?`)
-				message.channel.send(embed).then(msg => msg.delete(60000));
-			  } else if (pet_healthPercent < 11) {
-				const embed = new Discord.RichEmbed()
-				.setTitle(`__${user.username} PET Health Warning!!! - ${pet_healthPercent}%__`)
-				.setColor(0xff0000)
-				.setDescription(`**${user.username}** your pet is at __**${pet_currentHP}**__ health!!!\n`)
-				.setImage(`${senddeath}`)
-				.setFooter(`OMG YOUR GOING TO LET YOUR PET DIE????`)
-			  message.channel.send(embed).then(msg => msg.delete(60000));
-			  }
-			  else { 
-				var embed = new Discord.RichEmbed()
-				  .setAuthor(user.username, user.avatarURL)
-				  .addField(`__Character Health__ - **${char_healthPercent}%**`,`(${char_currentHP} HP / ${char_maxHP} HP)`, false)
-				  .setFooter(`&Config To Change Health Monitor Settings`)
-		
-				if (char_healthPercent <= 20 || pet_healthPercent <= 20)
-				  embed.setColor('RED');
-				else if (char_healthPercent <= 40 || pet_healthPercent <= 40)
-				  embed.setColor('ORANGE');
-				else if (char_healthPercent <= 60 || pet_healthPercent <= 60)
-				  embed.setColor('GOLD');
-				else if (char_healthPercent <= 100 || pet_healthPercent <= 100)
-				  embed.setColor('GREEN');
-		
-				if (!isNaN(pet_healthPercent))
-				  embed.addField(`__Pet Health__ - **${pet_healthPercent}%**`, `(${pet_currentHP} HP / ${pet_maxHP} HP)`, false);
-				message.channel.send({embed}).then(msg => msg.delete(30000));
-			  }
-			}
-			console.log(char_healthPercent + " " +pet_healthPercent)
-			if (Object.keys(result).length !== 0) {
-			  let settings = JSON.parse(result[0].settings);
-			  if (settings.healthMonitor !== 'off') {
-				if (settings.healthMonitor === 'on' || char_healthPercent < settings.healthMonitor || pet_healthPercent < settings.healthMonitor) {
-				  drpg();
-				  console.log(char_healthPercent + " " +pet_healthPercent)
-				}
-			  }
-			}
-		  });
-		}
-	}
-  })
+			if (message.embeds[0].author != undefined) {
+				if (message.embeds[0].author.name.indexOf("Adventure") != -1) {
+					const char_field = message.embeds[0].fields[1].name == "Critical Hit!" ? message.embeds[0].fields[4] : message.embeds[0].fields[3];
+					player.currentHP = parseInt(char_field.value.split(' ')[1].split('/')[0].replace(',', ''));
+					player.maxHP = parseInt(char_field.value.split(' ')[1].split('/')[1].replace(',', ''));
 
+					const pet_field = message.embeds[0].fields[1].name == "Critical Hit!" ? message.embeds[0].fields[3] : message.embeds[0].fields[2];
+					pet.currentHP = parseInt(pet_field.value.split(' ')[1].split('/')[0].replace(',', ''));
+					pet.maxHP = parseInt(pet_field.value.split(' ')[1].split('/')[1].replace(',', ''));
+
+					player.name = char_field.name;
+				}
+			}
+		}
+
+		const user = client.users.find('username', player.name);
+		player.healthPercent = Math.round(10 * player.currentHP * 100 / player.maxHP) / 10;
+		pet.healthPercent = Math.round(10 * pet.currentHP * 100 / pet.maxHP) / 10;
+		if (user != null && player.name != undefined && player.healthPercent != pet.healthPercent) {
+			const execute = (individual, healthMonitor) => {
+				const playerWarning = () => {
+					const embed = new Discord.RichEmbed()
+					  	.setTitle(`__${user.username} Health Warning!!! - ${player.healthPercent}%__`)
+						.setColor(0xff0000)
+						.setDescription(`**${user.username}** is at __**${player.currentHP}**__ health!!!\n`)
+						.setImage(`${senddeath}`)
+						.setFooter(`You are going to die aren't you?`)
+					message.channel.send(embed).then(msg => msg.delete(60000));
+				}, petWarning = () => {
+					const embed = new Discord.RichEmbed()
+						.setTitle(`__${user.username} PET Health Warning!!! - ${pet.healthPercent}%__`)
+						.setColor(0xff0000)
+						.setDescription(`**${user.username}** your pet is at __**${pet.currentHP}**__ health!!!\n`)
+						.setImage(`${senddeath}`)
+						.setFooter(`OMG YOUR GOING TO LET YOUR PET DIE????`)
+				  message.channel.send(embed).then(msg => msg.delete(60000));
+				}, general = () => {
+					var embed = new Discord.RichEmbed()
+					 	.setAuthor(user.username, user.avatarURL)
+					 	.addField(`__Character Health__ - **${player.healthPercent}%**`,`(${player.currentHP} HP / ${player.maxHP} HP)`, false)
+					 	.setFooter(`&config To Change Health Monitor Settings`)
+	  
+					if (player.healthPercent <= 20 || pet.healthPercent <= 20) embed.setColor('RED');
+					else if (player.healthPercent <= 40 || pet.healthPercent <= 40) embed.setColor('ORANGE');
+					else if (player.healthPercent <= 60 || pet.healthPercent <= 60) embed.setColor('GOLD');
+					else if (player.healthPercent <= 100 || pet.healthPercent <= 100) embed.setColor('GREEN');
+					if (!isNaN(pet.healthPercent)) embed.addField(`__Pet Health__ - **${pet.healthPercent}%**`, `(${pet.currentHP} HP / ${pet.maxHP} HP)`, false);
+					message.channel.send({embed}).then(msg => msg.delete(30000));
+				}
+
+				if (['character', "pet"].indexOf(individual) !== -1) {
+					if (individual === 'character' && player.healthPercent < 11 && (player.healthPercent <= healthMonitor || healthMonitor === 'on')) return playerWarning();
+					if (individual === 'pet' && pet.healthPercent < 11 && (pet.healthPercent <= healthMonitor || healthMonitor === 'on')) return petWarning();
+					if (player.healthPercent <= healthMonitor || pet.healthPercent <= healthMonitor || healthMonitor === 'on') return general();
+				} else {
+					if (player.healthPercent < 11 && (player.healthPercent <= healthMonitor || healthMonitor === 'on')) return playerWarning();
+					else if (pet.healthPercent < 11 && (pet.healthPercent <= healthMonitor || healthMonitor === 'on')) return petWarning();
+					else if (player.healthPercent <= healthMonitor || pet.healthPercent <= healthMonitor || healthMonitor === 'on') return general();
+				}
+			}
+
+			poolQuery(`SELECT * FROM users WHERE userId='${user.id}'`).then((result) => {
+				if (result[0] !== undefined) {
+					if (JSON.parse(result[0].settings).healthMonitor === 'off') return;
+					execute(JSON.parse(result[0].settings).individualHealthMonitor, JSON.parse(result[0].settings).healthMonitor);
+				}
+			});
+		}
+  	});
 }
