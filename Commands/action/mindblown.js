d17b5f24cd2f8ea118b02c683773a9d5810b501e
mount2010 <mount201076@gmail.com>
2018-08-10 20:43:15 +0800

    add mindblown command

---
+++ Commands/action/mindblown.js
@@ -0,0 +1,22 @@
+exports.run = (bot, message, args) => {
+    const images = require("./../../Data/imageurls.json");
+    const randomGif = images.mindblown[~~(Math.random() * images.mindblown.length)];
+    
+    message.channel.send({embed:{
+        author: {
+            name: message.author.username,
+            icon_url: message.author.avatarURL
+        },
+        description: `${message.author}'s mind has been blown`,
+        image: {
+            url: randomGif
+        },
+        timestamp: new Date()
+    }});
+}
+exports.infos = {
+    category: "Image",
+    description: "Shows a GIF showing everyone how much your mind was blown",
+    usage: "\`&mindblown",
+    example: "\`&mindblown\`"
+}
