83a34c667a194a7e0248a88f353243ead8ceec19
Nightmare <33561320+Nightmare2112@users.noreply.github.com>
2018-08-17 04:51:46 -0400

    Update info.js

--- Commands/general/info.js
+++ Commands/general/info.js
@@ -9,7 +9,7 @@ exports.run = function(bot, message, args) {
     const embed = new Discord.RichEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL)
         .setTitle('Details About Aldebaran Bot')
-        .addField(`__Developers__`, `<@>310296184436817930\n<@143026985763864576>\n<@320933389513523220>`, true)
+        .addField(`__Developers__`, `<@310296184436817930>\n<@320933389513523220>\n<@143026985763864576>`, true)
         .addField(`__Prefix__`, `**&**`, true)
         .addField(`__Statistics__`, `**Servers** : ${bot.guilds.size}\n**Channels** : ${bot.channels.size}\n**Users** : ${bot.users.size}`, true)
         .addField(`__Resources__`, `**Memory Usage** : ${Math.round(100 * process.memoryUsage().heapTotal/1000000) / 100} MB\n**Uptime** : ${Math.floor(bot.uptime/3600000)}h${minutesUptime.length == 2 ? '0' + minutesUptime : minutesUptime}`, true)
