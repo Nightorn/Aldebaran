a66661c868a8417cad7a86e4674b6b34f372e0a5
Ciborn <rb.ciborn+dev@gmail.com>
2018-09-01 10:28:45 +0200

    update: improve osu! beatmap retrieval

--- functions/osu!/retrieveBeatmapFile.js
+++ functions/osu!/retrieveBeatmapFile.js
@@ -1,11 +1,11 @@
 module.exports = (beatmapId) => {
     return new Promise((resolve, reject) => {
         const fs = require('fs');
-        if (!fs.existsSync(`./osu/`)) fs.mkdirSync(`./osu/`);
-        if (fs.existsSync(`./osu/${beatmapId}.osu`)) return resolve();
+        if (!fs.existsSync(`./cache/osu!/`)) fs.mkdirSync(`./cache/osu!/`);
+        if (fs.existsSync(`./cache/osu!/${beatmapId}.osu`)) return resolve();
         const fetch = require('node-fetch');
         fetch(`https://bloodcat.com/osu/b/${beatmapId}`).then(res => {
-            const dest = fs.createWriteStream(`./osu/${beatmapId}.osu`);
+            const dest = fs.createWriteStream(`./cache/osu!/${beatmapId}.osu`);
             res.body.pipe(dest);
             res.body.on('error', reject);
             dest.on('finish', resolve);
