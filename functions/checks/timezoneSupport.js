d4737a71f6c5aaad71b24d9e79aa580f46dae272
Ciborn <rb.ciborn+dev@gmail.com>
2018-09-01 10:29:15 +0200

    update: move timezone check

---
+++ functions/checks/timezoneSupport.js
@@ -0,0 +1,6 @@
+const timeNames = require('moment-timezone').tz.names();
+module.exports = (value) => {
+    if (/^GMT(\+|-)\d{1,2}/i.test(value)) return true;
+    else if (timeNames.indexOf(value) !== -1) return true;
+    else return false;
+}
