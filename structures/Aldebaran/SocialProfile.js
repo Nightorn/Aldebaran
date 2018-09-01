d9413624462ed4a94cd77d7fab8fa98faccfe361
Ciborn <rb.ciborn+dev@gmail.com>
2018-09-01 10:12:01 +0200

    add(user): integrate social profile to user

---
+++ structures/Aldebaran/SocialProfile.js
@@ -0,0 +1,36 @@
+module.exports = class SocialProfile {
+    constructor(user) {
+        this.user = user;
+        this.client = this.user.client;
+        this.client.database.socialprofile.selectOneById(this.user.id).then(profile => {
+            if (profile !== undefined) return this.build(profile);
+            this.existsInDB = false;
+        });
+    }
+
+    build(data) {
+        for (let [key, value] of Object.entries(data)) this[key] = value;
+        this.existsInDB = true;
+    }
+
+    async create() {
+        this.existsInDB = true;
+        return await this.client.database.socialprofile.createOneById(this.user.id);
+    }
+
+    async clear() {
+        this.existsInDB = false;
+        return await this.client.database.socialprofile.deleteOneById(this.user.id);
+    }
+
+    async changeProperty(property, value) {
+        return new Promise(async (resolve) => {
+            if (!this.existsInDB) await this.create();
+            this.client.database.socialprofile.updateOneById(this.user.id, new Map([[property, value]])).then(async (result) => {
+                const newProfile = await this.client.database.socialprofile.selectOneById(this.user.id);
+                this.build(newProfile);
+                resolve(result);
+            });
+        });
+    }
+}
