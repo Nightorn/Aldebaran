4a89913c04c82fa757adf5c5893d1e980da73d05
Ciborn <rb.ciborn+dev@gmail.com>
2018-09-01 10:33:45 +0200

    event(ready): send message to channel on start up

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,13 @@
-
+const { MessageEmbed } = require('discord.js');
 exports.run = (client) => {
+	const embed = new MessageEmbed()
+		.setTitle(`Started ${client.user.username}`)
+		.setDescription(`**Uptime** : ${Math.floor(client.uptime)}ms`)
+		.setColor(`BLUE`);
+	client.guilds.get('461792163525689345').channels.get('485023018045669396').send({embed});
+
 	if (process.argv.indexOf('dev') !== -1) {
-		console.log(`Bot has started in dev mode with prefix ${process.argv[3]}, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+		console.log(`Bot has started in dev mode with prefix ${process.argv[3] || client.config.prefix}, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 		client.user.setActivity(`Aldebaran Developer mode`);
 	} else {
 		console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
