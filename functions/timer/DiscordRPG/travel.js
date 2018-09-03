e035848137285397a914f56881ad196d832d26a6
Ciborn <rb.ciborn+dev@gmail.com>
2018-09-03 10:32:55 +0200

    fix: timers

--- functions/timer/DiscordRPG/travel.js
+++ functions/timer/DiscordRPG/travel.js
@@ -1,33 +1,21 @@
-const Discord = require("discord.js");
-const locationinfo = require(`${process.cwd()}/Data/drpglocationlist.json`)
+const locationinfo = require(`${process.cwd()}/Data/drpglocationlist.json`);
 module.exports = function(bot, message) {
-    if (message.content.indexOf("started their journey to") !== -1){
-        var locationName = message.content.slice(message.content.indexOf("to")+2).trim().split("!");
-        var userName = message.content.substring(0,message.content.indexOf("started")).trim()
-        const user = bot.users.find('username',userName)
-        const timer = function(){
-            if (bot.traveltimer.has(user.id))return;
-            poolQuery(`SELECT * FROM users WHERE userId= '${user.id}'`).then((result) =>{
-                for (var i = 1; i < 46; i++){
-                   if (locationinfo[i].name == locationName[0]){
-                        if(locationinfo[i].traveltime !== undefined){
-                            if (Object.keys(result).length != 0){
-                                let settings = JSON.parse(result[0].settings)
-                                if (settings.travelTimer === `on`){
-                                    bot.traveltimer.set(user.id)
-                                    setTimeout((channel, userid) => {
-                                        message.channel.send(`<@${user.id}> has arrived at ${locationName} better lock your doors!!`)
-                                        bot.traveltimer.delete(user.id)
-                                    },locationinfo[i].traveltime * 1000, message.channel, user.id)
-                                }
-                            }
-                        }
-                    }
+    const userName = message.content.substring(0, message.content.indexOf("started")).trim();
+    const user = bot.users.find(u => u.username === userName);
+    if (user !== undefined) {
+        if (user.timers.travel !== null && user.settings.travelTimer === 'on') return;
+        if (message.content.indexOf("started their journey to") !== -1) {
+            var locationName = message.content.slice(message.content.indexOf("to")+2).trim().split("!");
+            for (let [id, data] of Object.entries(locationinfo)) {
+                if (data.name === locationName[0] && data.traveltime !== undefined) {
+                    user.timers.travel = setTimeout((locationName, message, user) => {
+                        user.timers.travel = null;
+                        message.channel.send(`<@${user.id}> has arrived at ${locationName} better lock your doors!!`);
+                    }, data.traveltime * 1000, locationName, message, user);
                 }
-            }).catch()
-        }                            
-        timer()
-    } else return;
+            }
+        }
+    }
 }
 
     
