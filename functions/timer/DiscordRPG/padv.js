c18c2f895b828dcfe69149d708b30a3231c7d0bd
Ciborn <rb.ciborn+dev@gmail.com>
2018-09-11 21:21:21 +0200

    fix: padv conflicting with adv timer

--- functions/timer/DiscordRPG/padv.js
+++ functions/timer/DiscordRPG/padv.js
@@ -3,7 +3,7 @@ module.exports = function(message){
 	if (message.guild.settings.adventureTimer === 'on' && message.author.settings.adventureTimer === 'on') {
 		if (message.content.toLowerCase().startsWith(`#!padv`) || message.content.toLowerCase().startsWith(`,padv`)||message.content.toLowerCase().startsWith(`.padv`)) {
 			if (message.guild.settings.autoDelete !== 'off') message.delete({ timeout: 1000 });
-			message.author.timers.adventure = setTimeout(() => {
+			message.author.timers.padventure = setTimeout(() => {
                 message.channel.send("<@" + message.author.id + "> Party Time! :tada:").then(msg => {
 					if (message.guild.settings.autoDelete !== 'off') message.delete({ timeout: 10000 });
 				});
