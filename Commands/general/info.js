445146c1cf96b4dac3149ba8c331e67ba22a7526
Ciborn <rb.ciborn+dev@gmail.com>
2018-10-04 21:18:26 +0200

    update(info): add DiscordRPG note

--- Commands/general/info.js
+++ Commands/general/info.js
@@ -11,6 +11,7 @@ exports.run = function(bot, message, args) {
         .addField(`__Statistics__`, `**Servers** : ${bot.guilds.size}\n**Channels** : ${bot.channels.size}\n**Users** : ${bot.users.size}`, true)
         .addField(`__Resources__`, `**Memory Usage** : ${Math.round(100 * process.memoryUsage().heapTotal/1000000) / 100} MB\n**Uptime** : ${Math.floor(bot.uptime/3600000)}h${minutesUptime.length == 2 ? '0' + minutesUptime : minutesUptime}`, true)
         .addField(`__Powered By__`, `**DigitalOcean** : VPS Host\n**Node.JS** : JavaScript Runtime`, true)
+        .addField(`__Note__`, `Please note that ${bot.user.username} is not directly related to DiscordRPG. If you have any question, suggestion or bug report that concerns Aldebaran and not DiscordRPG, please join [our server](https://discord.gg/3x6rXAv) or use \`&suggest\` and \`&bugreport\`.`)
         .setFooter(`The prefix in this guild is "${message.guild.prefix}".`)
         .setThumbnail(message.guild.iconURL())
         .setColor(`GREEN`);
