0bddfe1e1f1702145139ca7bddac4591ea512b00
Ciborn <rb.ciborn+dev@gmail.com>
2018-10-04 21:17:42 +0200

    feat(ready): auto tickets channels fetching

--- events/ready.js
+++ events/ready.js
@@ -6,8 +6,12 @@ exports.run = (client) => {
 		.setColor(`BLUE`);
 	client.guilds.get('461792163525689345').channels.get('485023018045669396').send({embed});
 
-	if (process.argv.indexOf('dev') !== -1) {
-		console.log(`Bot has started in dev mode with prefix ${process.argv[3] || client.config.prefix}, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+	client.guilds.get('461792163525689345').channels.get('461802546642681872').messages.fetch(200);
+	client.guilds.get('461792163525689345').channels.get('463094132248805376').messages.fetch(200);
+	client.guilds.get('461792163525689345').channels.get('494129501077241857').messages.fetch(200);
+
+	if (client.debugMode) {
+		console.log(`Bot has started in dev mode with prefix ${client.config.prefix}, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 		client.user.setActivity(`Aldebaran Developer mode`);
 	} else {
 		console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
