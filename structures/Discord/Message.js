56694fba5275e6914ef600fb2bbf3c6020152a6c
Ciborn <rb.ciborn+dev@gmail.com>
2018-11-27 18:34:47 +0100

    fix(struct): Discord.Message.userMentions

--- structures/Discord/Message.js
+++ structures/Discord/Message.js
@@ -3,7 +3,11 @@ module.exports = (BaseMessage) => {
     return class Message extends BaseMessage {
         constructor(client, data, channel) {
             super(client, data, channel);
-            this.userMentions = new Discord.GuildMemberStore(this.guild).filter(member => this.content.splice(this.guild.prefix.length).match(/(<@)([0-9]{16,19})>/g).indexOf(`<@${member.id}>`) !== -1);
+            this.userMentions = new Discord.Collection(this.guild.members.entries()).filter(member => {
+                const results = this.content.match(/(<@)([0-9]{16,19})>/g);
+                if (results !== null) return results.indexOf(`<@${member.id}>`) !== -1;
+            });
+            console.log(this.userMentions);
         }
     }
 }
