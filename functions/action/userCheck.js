3078dc34a25d88c784b8d8d3c2a05684a3294939
Ciborn <rb.ciborn+dev@gmail.com>
2019-01-01 15:38:18 +0100

    fix(userCheck): not handling correctly invalid users

--- functions/action/userCheck.js
+++ functions/action/userCheck.js
@@ -5,12 +5,10 @@ module.exports = function(bot, message, args) {
         } else if (args[0] !== undefined) {
             const userId = args[0];
             bot.users.fetch(userId).then((user) => {
-                if (user !== undefined) {
-                    resolve(userId);
-                } else {
-                    reject(new RangeError('Invalid User ID'));
-                }
-            });
+                resolve(userId);
+            }).catch(() => {
+                reject(new RangeError('Invalid User ID'));
+            })
         } else {
             const userId = message.author.id;
             resolve(userId);
