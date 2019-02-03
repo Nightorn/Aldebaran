87e411d4a96ce7bd88225318c579e20f555d7ff0
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-03 17:22:10 +0100

    fix: roles back in user command

--- Commands/general/user.js
+++ Commands/general/user.js
@@ -3,7 +3,7 @@ const userCheck = require('./../../functions/action/userCheck')
 exports.run = (bot, message, args) => {
     userCheck(bot, message, args).then(async (userId) => {
         bot.guilds.get(message.guild.id).members.fetch({user: userId}).then(user => {
-            var /*allRoles = [],*/ allPermissions = [];
+            var allRoles = new Map(), rolesList = [], allPermissions = [];
             const mjd = new Date(user.joinedTimestamp);
             if (user.permissions.has('ADMINISTRATOR')) allPermissions.push('Administrator');
             else for (let [name, value] of Object.entries(user.permissions.serialize())) if (value) {
@@ -12,23 +12,27 @@ exports.run = (bot, message, args) => {
                 for (let word of name) words.push(word[0].toUpperCase() + word.slice(1));
                 allPermissions.push(words.join(' '));
             };
-            //for (let [id, data] of user.roles) if (id !== user.highestRole.id && data.name !== '@everyone') allRoles.push(`<@&${id}>`);
+            for (let [id, data] of user.roles) if (data.name !== '@everyone') allRoles.set(id, data.rawPosition);
+            allRoles[Symbol.iterator] = function* () { yield* [...this.entries()].sort((a, b) => b[1] - a[1]) };
+            for (let [id, position] of allRoles) rolesList.push(`<@&${id}>`);
+            
+            const f = (data) => { return String(data).length === 1 ? `0${data}` : data }
+            const getDate = (time, md) => { return `${f(date.getMonth() + 1)}/${f(date.getDate())}/${f(date.getFullYear())} at ${f(date.getHours())}:${f(date.getMinutes())} UTC`; }
     
             const embed = new MessageEmbed()
-                .setAuthor(user.user.tag, user.user.avatarURL())
-                .setTitle(`User Details of ${user.user.username} in the ${message.guild.name} server`)
+                .setAuthor(`${user.user.tag}  |  ${message.guild.name}  |  Member Details`, user.user.avatarURL())
                 .setDescription(`**User ID** ${user.user.id}\n**Nickname** ${user.displayName}\n**Server Join Date** ${mjd.getUTCMonth()+1}/${mjd.getUTCDate()}/${mjd.getUTCFullYear()} ${mjd.getUTCHours()}:${mjd.getUTCMinutes()} UTC`)
-                //.addField(`Roles`, `**Highest Role** <@&${user.highestRole.id}>\n**Others** ${allRoles.join(', ')}`)
+                .addField(`Roles`, `${rolesList.join(', ')}`)
                 .addField(`Permissions`, allPermissions.join(', '))
                 .setThumbnail(user.user.avatarURL())
                 .setColor(user.displayColor)
                 .setFooter(`User Account Created on`)
                 .setTimestamp(new Date(user.user.createdTimestamp));
             message.channel.send({embed});
-        }).catch(() => {
+        }).catch((err) => {
             message.channel.send('Oops, looks like we got an error when fetching the requested data...');
         });
-    }).catch(err =>{
+    }).catch(err => {
         throw err;
     })
 }   
