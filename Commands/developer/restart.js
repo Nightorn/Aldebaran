39abbea4956c6723a6b47936382aa71c597eaada
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-03 17:21:10 +0100

    fix: restart not usable by anyone anymore

--- Commands/developer/restart.js
+++ Commands/developer/restart.js
@@ -1,6 +1,7 @@
 const AldebaranClient = require(`${process.cwd()}/structures/Discord/Client.js`);
 const { MessageEmbed } = require('discord.js');
 exports.run = function(bot, message, args) {
+    if (message.author.id !== '320933389513523220' && message.author.id !== '310296184436817930') return;
     const embed = new MessageEmbed()
         .setTitle(`Restarting ${bot.user.username}`)
         .setDescription(`Restarting the bot, depending on the bot size, this should take a while.`)
