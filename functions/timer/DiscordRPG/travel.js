b1b1049e8e65c948a6a49fbc8c678021dc25ff94
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-03 17:25:29 +0100

    fix: user confounding in travel timer

--- functions/timer/DiscordRPG/travel.js
+++ functions/timer/DiscordRPG/travel.js
@@ -1,19 +1,23 @@
 const locationinfo = require(`${process.cwd()}/Data/drpglocationlist.json`);
 module.exports = function(bot, message) {
-    const userName = message.content.substring(0, message.content.indexOf("started")).trim();
-    const user = bot.users.find(u => u.username === userName);
-    if (user !== undefined) {
-        if (user.timers.travel !== 'on') return;
-        if (message.content.indexOf("started their journey to") !== -1) {
-            var locationName = message.content.slice(message.content.indexOf("to")+2).trim().split("!");
-            for (let [id, data] of Object.entries(locationinfo)) {
-                if (data.name === locationName[0] && data.traveltime !== undefined) {
-                    user.timers.travel = setTimeout((locationName, message, user) => {
-                        user.timers.travel = null;
-                        message.channel.send(`<@${user.id}> has arrived at ${locationName} better lock your doors!!`).then(msg => {
-                            if (message.guild.settings.autoDelete === 'on') msg.delete({ timeout: 60000 });
-                        });
-                    }, data.traveltime * 1000, locationName, message, user);
+    if (message.author.id !== '170915625722576896') return;
+    if (message.content.indexOf("started their journey to") !== -1) {
+        const userName = message.content.substring(0, message.content.indexOf("started")).trim();
+        const matchedMessage = message.channel.messages.find(msg => msg.author.username === 'Ciborn');
+        if (matchedMessage !== undefined) {
+            const user = matchedMessage.author;
+            if (user !== undefined) {
+                if (user.timers.travel !== null) return;
+                var locationName = message.content.slice(message.content.indexOf("to")+2).trim().split("!");
+                for (let [id, data] of Object.entries(locationinfo)) {
+                    if (data.name === locationName[0] && data.traveltime !== undefined) {
+                        user.timers.travel = setTimeout((locationName, message, user) => {
+                            user.timers.travel = null;
+                            message.channel.send(`<@${user.id}> has arrived at ${locationName} better lock your doors!!`).then(msg => {
+                                if (message.guild.settings.autoDelete === 'on') msg.delete({ timeout: 60000 });
+                            });
+                        }, data.traveltime * 1000, locationName, message, user);
+                    }
                 }
             }
         }
