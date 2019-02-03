077475afea01f6e445a454a6f6be45b3266af95d
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-03 17:27:44 +0100

    fix: crash on guild join

--- structures/Discord/Message.js
+++ structures/Discord/Message.js
@@ -3,6 +3,8 @@ module.exports = (BaseMessage) => {
     return class Message extends BaseMessage {
         constructor(client, data, channel) {
             super(client, data, channel);
+            if (this.guild === undefined) return;
+            if (this.guild.members === undefined) return;
             this.userMentions = new Discord.Collection(this.guild.members.entries()).filter(member => {
                 const results = this.content.match(/(<@)([0-9]{16,19})>/g);
                 if (results !== null) return results.indexOf(`<@${member.id}>`) !== -1;
