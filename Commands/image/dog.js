bee93ad042acc24ddc68bd7c9d8c0ff660a609cf
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-03 14:23:26 +0100

    update(command): new dog API (dog.ceo)

--- Commands/image/dog.js
+++ Commands/image/dog.js
@@ -1,14 +1,14 @@
 exports.run = (bot, message, args) => {
     const request = require(`request`)
     const { MessageEmbed } = require(`discord.js`);
-    request({uri: `https://random.dog/woof.json`} , function (err, response, body) {
-        if (err) return message.channel.send("There seems to be a dogo problem");
+    request({uri: `https://dog.ceo/api/breeds/image/random`} , function (err, response, body) {
+        if (err) return message.channel.send("There seems to be a doggo problem.");
         const embed = new MessageEmbed()
             .setTitle(`**__Woof Woof__**`)
             .setAuthor(message.author.username,message.author.avatarURL())
             .setColor(0x00AE86)
-            .setImage(JSON.parse(body).url)
-            .setFooter(`Dogo Powered By: http://random.dog`)
+            .setImage(JSON.parse(body).message)
+            .setFooter(`Doggo Powered By: http://dog.ceo`)
         message.channel.send({embed});
     });
 }
