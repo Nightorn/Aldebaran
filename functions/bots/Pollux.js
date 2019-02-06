7278084cde8b5b8db1d3b8dfeff2e4fdea0b8374
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-06 17:13:14 +0100

    fix: incorrect validation in Pollux.js

--- functions/bots/Pollux.js
+++ functions/bots/Pollux.js
@@ -1,14 +1,16 @@
 module.exports = function(bot, message, args) {
-	if (message.guild.polluxBoxPing.size === 0) {
-		for (let [id, member] of message.guild.members) {
-			if (member.user.settings.polluxBoxPing === 'on') message.guild.polluxBoxPing.set(id, member.user);
+	if (message.guild.settings.polluxBoxPing === 'on') {
+		if (message.guild.polluxBoxPing.size === 0) {
+			for (let [id, member] of message.guild.members) {
+				if (member.user.settings.polluxBoxPing === 'on') message.guild.polluxBoxPing.set(id, member.user);
+			}
 		}
+		if (message.content.endsWith(`a chance to claim it!`) && message.author.id == "271394014358405121") {
+			var list = "Come grab my box!\n ";
+			for (let [id, user] of message.guild.polluxBoxPing) {
+				list += `<@${id}> `;
+			}
+			message.channel.send(list);
+		  }
 	}
-    if (message.content.endsWith(`a chance to claim it!`) && message.author.id == "271394014358405121") {
-		var list = "Come grab my box!\n ";
-		for (let [id, user] of message.guild.polluxBoxPing) {
-			list += `<@${id}> `;
-		}
-		message.channel.send(list);
-    }
 }
