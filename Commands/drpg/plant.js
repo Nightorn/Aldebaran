9ea3dc664be2e7ab6464af474096a419bc049aef
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-17 18:51:18 +0100

    update(command): &plant

--- Commands/drpg/plant.js
+++ Commands/drpg/plant.js
@@ -1,51 +1,49 @@
 const { MessageEmbed } = require('discord.js');
 exports.run = (bot, message, args) => {
     const request = require('request');
-    const itemlist = require(`${process.cwd()}/Data/drpgitemlist.json`);
+    const itemlist = require(`${process.cwd()}/Data/drpgitemlist.json`).data;
     const locationdb = require(`${process.cwd()}/Data/drpglocationlist.json`);
     require(`${process.cwd()}/functions/action/userCheck.js`)(bot, message, args).then(usrid => {
-        request({uri:`https://api.discorddungeons.me/v3/user/${usrid}`, headers: {"Authorization" : bot.config.drpg_apikey} }, function(err, response, body) {
+        request({uri:`https://api.discorddungeons.me/v3/user/${usrid}`, headers: {"Authorization" : bot.config.drpg_apikey} }, async function(err, response, body) {
             if (err) return;
             const data = JSON.parse(body).data;
+            const user = await bot.users.fetch(usrid);
             if (data.location === undefined) return message.channel.send(`Hey **${data.name}**, travel somewhere and set a trap on your way!`);
             if (data.location.saplings === null || data.location.saplings === undefined) return message.channel.send(`Hey **${data.name}**, please plant some saplings at your purchased fields before!`);
             //if (Object.values(data.location.saplings).indexOf(null) != -1) return message.channel.send(`**Error** No Currently Planted Saplings Found`);  
                 //Caused errors when any location was "null" would then show complete message and ignore all other locations was removed fixing error.
             const embed = new MessageEmbed()
-                .setTitle(data.name + "'s Planting Info")
-                .setAuthor(message.author.username,message.author.avatarURL())
+                .setAuthor(`${user.username}  |  Plant Informations`, user.avatarURL())
                 .setColor(0x00AE86)
-                .setDescription(`**Please note all infomation about amounts are strictly estimates!!**`)
+                .setFooter(`Please note that all amounts of items shown here are estimations.`)
                 
-            for (let [key, value] of Object.entries(data.location.saplings)){
-                var planttime = 0
-                var checktime = 0
-                var plantname = `Nothting`
-                var plantdate = `Not yet planted`
-                var lootamountcurrentmin = `NA`
-                var lootamountcurrentmax = `NA`
-                var lootamountmaxmin = `NA`
-                var lootamountmaxmax = `NA`
-                var currentreap = data.attributes.reaping !== 0 ? data.attributes.reaping : 1;
-                var maxreap = Math.round(data.level * 5);
+            
+            const f = (data) => { return String(data).length === 1 ? `0${data}` : data }
+            const getDate = (time) => {
+                const date = new Date(time);
+                return `**${f(date.getMonth() + 1)}/${f(date.getDate())}/${f(date.getFullYear())}** at **${f(date.getHours())}:${f(date.getMinutes())}** UTC`;
+            }
 
-                if (value !== null){
-                    plantdate = new Date(value.time);
-                    planttime = Math.round((new Date()-value.time)/86400000);
-                    checktime = Math.round((new Date()-value.time)/1000);
-                    lootamountcurrentmin = Math.floor(1+((Math.floor(currentreap))*(checktime/25)/15000));
-                    lootamountcurrentmax = Math.floor(1+((Math.floor(currentreap))*(checktime/25)/14000));
-                    lootamountmaxmin = Math.floor(1+(maxreap*(checktime/25)/15000));
-                    lootamountmaxmax = Math.floor(1+(maxreap*(checktime/25)/14000));
-                    var plantid = value.id 
+            for (let [key, value] of Object.entries(data.location.saplings)) {
+                if (value !== null) {
+                    var currentreap = data.attributes.reaping !== 0 ? data.attributes.reaping : 1;
+                    var maxreap = Math.round(data.level * 5);
+                    var plantdate = new Date(value.time);
+                    var planttime = Math.round((new Date()-value.time)/86400000);
+                    var checktime = Math.round((new Date()-value.time)/1000);
+                    var lootamountcurrentmin = Math.floor(1+((Math.floor(currentreap))*(checktime/25)/15000));
+                    var lootamountcurrentmax = Math.floor(1+((Math.floor(currentreap))*(checktime/25)/14000));
+                    var lootamountmaxmin = Math.floor(1+(maxreap*(checktime/25)/15000));
+                    var lootamountmaxmax = Math.floor(1+(maxreap*(checktime/25)/14000));
+
                     for (var i = 0; i < itemlist.length; i++){
                         if (itemlist[i].id == value.id){
                             plantname = itemlist[i].name  
                         }
                     }
-                };    
-                var locationname = locationdb[`${key}`] !== undefined ? locationdb[`${key}`].name : "???"
-                embed.addField(`__${locationname} - Set ${planttime} days ago.__`,`Seed Planted: **${plantname}**\nPlanted Since: ${plantdate}\nChecked with **Current** reaping would give you **${lootamountcurrentmin} - ${lootamountcurrentmax} items**.\nChecked with **Max** reaping would give you **${lootamountmaxmin} - ${lootamountmaxmax} items**.`,false);
+                    var locationname = locationdb[`${key}`] !== undefined ? locationdb[`${key}`].name : "???"
+                    embed.addField(`${plantname} @ ${locationname} - ${planttime} days old`,`Planted the ${getDate(plantdate)}.\nWith your **current** reaping skills, you would get between **${lootamountcurrentmin}** and **${lootamountcurrentmax}** items.\nWith the **highest** reaping skills, you would get between **${lootamountmaxmin}** and **${lootamountmaxmax}** items.`, false);
+                }
             }
             message.channel.send({embed});
         });
