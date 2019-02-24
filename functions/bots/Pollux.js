b43df83b4c9b22ea55146a115dd8faf36c875f93
Ciborn <rb.ciborn+dev@gmail.com>
2019-02-24 17:29:04 +0100

    fix: online status check in Pollux box pings

--- functions/bots/Pollux.js
+++ functions/bots/Pollux.js
@@ -2,7 +2,7 @@ module.exports = function(bot, message, args) {
 	if (message.guild.settings.polluxBoxPing === 'on') {
 		if (message.guild.polluxBoxPing.size === 0) {
 			for (let [id, member] of message.guild.members) {
-				if (member.user.settings.polluxBoxPing === 'on') message.guild.polluxBoxPing.set(id, member.user);
+				if (member.user.settings.polluxBoxPing === 'on' && member.user.presence.status !== 'offline') message.guild.polluxBoxPing.set(id, member.user);
 			}
 		}
 		if (message.content.endsWith(`a chance to claim it!`) && message.author.id == "271394014358405121") {
