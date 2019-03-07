dff7f74cb027a4b1143ae6fd79752f0eea0a94da
Ciborn <rb.ciborn+dev@gmail.com>
2019-03-07 13:56:20 +0100

    fix: adventure/sidesTimer conflicting with autoDelete

--- functions/timer/DiscordRPG/adv.js
+++ functions/timer/DiscordRPG/adv.js
@@ -1,13 +1,13 @@
 module.exports = function(message) {
 	if (message.author.timers.adventure !== null) return;
-	if (message.guild.settings.adventureTimer === 'on' && message.author.settings.adventureTimer === 'on') {
-		var prefix = null;
-		message.content = `${message.content} `;
-		for (let element of ["DiscordRPG", "#!", "<@170915625722576896>", message.guild.settings.discordrpgPrefix]) {
-			if (message.content.indexOf(`${element}adv `) === 0 || message.content.indexOf(`${element}adventure `) === 0) prefix = element;
-		}
-		if (prefix !== null) {
-			if (message.guild.settings.autoDelete === 'on') message.delete({ timeout: 1000 });
+	message.content = `${message.content.toLowerCase()} `;
+	var prefix = null;
+	for (let element of ["DiscordRPG", "#!", "<@170915625722576896>", message.guild.settings.discordrpgPrefix]) {
+		if (message.content.indexOf(`${element}adv `) === 0 || message.content.indexOf(`${element}adventure `) === 0) prefix = element;
+	}
+	if (prefix !== null) {
+		if (message.guild.settings.autoDelete === 'on') message.delete({ timeout: 1000 });
+		if (message.guild.settings.adventureTimer === 'on' && message.author.settings.adventureTimer === 'on') {
 			message.author.timers.adventure = setTimeout(() => {
 				message.channel.send(`<@${message.author.id}> adventure time! :crossed_swords:`).then(msg => {
 					if (message.guild.settings.autoDelete === 'on') msg.delete({ timeout: 10000 });
--- functions/timer/DiscordRPG/sides.js
+++ functions/timer/DiscordRPG/sides.js
@@ -1,49 +1,55 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = async function(message) {
-	if (message.author.timers.sides !== null) return;
-	if (message.guild.settings.sidesTimer === 'on' && message.author.settings.sidesTimer === 'on') {
-		var prefix = null;
-		message.content = `${message.content} `;
-		for (let element of ["DiscordRPG", "#!", message.guild.settings.discordrpgPrefix, message.guild.settings.discordrpgPrefix]) {
-			if (message.content.indexOf(`${element}mine `) === 0) prefix = element;
+	var prefix = null;
+	var sidesPass = false;
+	message.content = `${message.content.toLowerCase()} `;
+	for (let element of ["DiscordRPG", "#!", message.guild.settings.discordrpgPrefix]) {
+		for (let action of ['mine', 'forage', 'chop', 'fish']) {
+			if (message.content.indexOf(`${element}${action} `) === 0) prefix = element;
+			if (prefix !== null) if (message.content.indexOf(prefix + action) === 0) sidesPass = true;
 		}
+	}
+	if (message.guild.settings.autoDelete === 'on' && sidesPass) message.delete({ timeout: 500 });
+	if (message.content.indexOf(`${prefix}mine`) !== -1) {
+		if (message.author.timers.sides !== null) return;
 		if (prefix !== null) {
-			const emoji = ["🥕","🍋","🥔","🐟"];
-			var randomemoji = (`${emoji[~~(Math.random() * emoji.length)]}`);
-			if (message.guild.settings.autoDelete === 'on') message.delete({ timeout: 500 });
-			const timerEmbed = new MessageEmbed()
-				.setAuthor(message.author.username, message.author.avatarURL())
-				.setColor(0x00AE86)
-				.setDescription(`React with 🚫 to cancel timer.`)
-			message.channel.send({embed: timerEmbed}).then(mesg => {
-				mesg.react("🚫");
-				mesg.awaitReactions((reaction, user) => reaction.emoji.name === "🚫" && user.id == message.author.id, {time: 5000, max: 1}).then(reactions => {
-					mesg.delete({ timeout: 5000 });
-					if (reactions.get("🚫") === undefined) {
-						const embed = new MessageEmbed()
-							.setDescription(`Your sides timer has been set!`)
-							.setAuthor(message.author.username, message.author.avatarURL())
-							.setColor(0x00AE86);
-						message.channel.send({embed}).then(timerset => {
-							timerset.delete({ timeout: 5000 });
-							message.author.timers.sides = setTimeout(() => {
-								message.channel.send(`<@${message.author.id}> sides time! ${randomemoji}`).then(msg => {
-									if (message.guild.settings.autoDelete === 'on') msg.delete({ timeout: 180000 });
-								});
-								message.author.timers.sides = null;
-							}, 312500);
-						});
-					} else {
-						const embed1 = new MessageEmbed()
-							.setDescription(`Timer Canceled`)
-							.setAuthor(`${message.author.username}`)
-							.setColor(`RED`)
-						message.channel.send({embed: embed1}).then(timernotset => {
-							timernotset.delete({ timeout: 5000 });
-						});
-					}
+			if (message.guild.settings.sidesTimer === 'on' && message.author.settings.sidesTimer === 'on') {
+				const emoji = ["🥕","🍋","🥔","🐟"];
+				var randomemoji = (`${emoji[~~(Math.random() * emoji.length)]}`);
+				const timerEmbed = new MessageEmbed()
+					.setAuthor(message.author.username, message.author.avatarURL())
+					.setColor(0x00AE86)
+					.setDescription(`React with 🚫 to cancel timer.`)
+				message.channel.send({embed: timerEmbed}).then(mesg => {
+					mesg.react("🚫");
+					mesg.awaitReactions((reaction, user) => reaction.emoji.name === "🚫" && user.id == message.author.id, {time: 5000, max: 1}).then(reactions => {
+						mesg.delete({ timeout: 5000 });
+						if (reactions.get("🚫") === undefined) {
+							const embed = new MessageEmbed()
+								.setDescription(`Your sides timer has been set!`)
+								.setAuthor(message.author.username, message.author.avatarURL())
+								.setColor(0x00AE86);
+							message.channel.send({embed}).then(timerset => {
+								timerset.delete({ timeout: 5000 });
+								message.author.timers.sides = setTimeout(() => {
+									message.channel.send(`<@${message.author.id}> sides time! ${randomemoji}`).then(msg => {
+										if (message.guild.settings.autoDelete === 'on') msg.delete({ timeout: 180000 });
+									});
+									message.author.timers.sides = null;
+								}, 312500);
+							});
+						} else {
+							const embed1 = new MessageEmbed()
+								.setDescription(`Timer Canceled`)
+								.setAuthor(`${message.author.username}`)
+								.setColor(`RED`)
+							message.channel.send({embed: embed1}).then(timernotset => {
+								timernotset.delete({ timeout: 5000 });
+							});
+						}
+					});
 				});
-			});
+			}
 		}
 	}
 }     
