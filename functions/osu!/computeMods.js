a47f0b7544e2d06973f8ac7ae32f542b33877c15
Ciborn <rb.ciborn+dev@gmail.com>
2019-03-07 14:54:26 +0100

    update: ppv2 changes with osu!

--- functions/osu!/computeMods.js
+++ functions/osu!/computeMods.js
@@ -1,31 +1,33 @@
-module.exports = function(bitfield) {
-    const mods = {
-        PF: 16384,
-        SO: 4096,
-        FL: 1024,
-        NC: 512,
-        HT: 256,
-        RX: 128,
-        DT: 64,
-        SD: 32,
-        HR: 16,
-        HD: 8,
-        EZ: 2,
-        NF: 1,
-        None: 0
-    };
+module.exports.run = function(bitfield) {
+    const mods = module.exports.mods;
     var actualBitfield = bitfield;
     var foundMods = [];
 
     do {
         for (let [key, value] of Object.entries(mods)) {
             if (value <= actualBitfield) {
-                foundMods.push(key);
+                if (!(key === 'DT' && foundMods.indexOf('NC') !== -1)) foundMods.push(key);
                 actualBitfield -= value;
             }
         }
-    } while (actualBitfield != 0);
+    } while (actualBitfield !== 0);
 
-    foundMods != [] && foundMods.indexOf('None') != -1 ? foundMods.pop() : foundMods;
+    foundMods !== [] && foundMods.indexOf('None') !== -1 ? foundMods.pop() : foundMods;
     return foundMods;
+}
+
+module.exports.mods = {
+    PF: 16384,
+    SO: 4096,
+    FL: 1024,
+    NC: 512,
+    HT: 256,
+    RX: 128,
+    DT: 64,
+    SD: 32,
+    HR: 16,
+    HD: 8,
+    EZ: 2,
+    NF: 1,
+    None: 0
 }
