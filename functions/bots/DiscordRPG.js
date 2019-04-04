54a07851cb0028a96f679209963161e3d26c60f4
Ciborn <rb.ciborn+dev@gmail.com>
2019-04-04 17:21:12 +0200

    eslint: fix 340 errors on 3 files

--- functions/bots/DiscordRPG.js
+++ functions/bots/DiscordRPG.js
@@ -1,108 +1,229 @@
+/* eslint consistent-return: off */
 const Discord = require("discord.js");
-module.exports = function(client, message) {
-	if (message.guild.settings.healthMonitor === 'off') return;
-	var player = {
-		name : null,
-		currentHP : 0,
-		maxHP : 0
-	}, pet = {
-		currentHP : 0,
-		maxHP : 0
-	};
-	var deathimage = require(`./../../Data/imageurls.json`)
-	var senddeath = (`${deathimage.deathimage[~~(Math.random() * deathimage.deathimage.length)]}`);
-	if (message.embeds.length === 0) {
-		if (message.content.indexOf("'s Adventure") != -1) {
-			var healthMessagePattern = /( has [\d,]+\/[\d,]+ HP left\.)|(used .+? and got [\d,]+?HP\. \([\d,]+\/[\d,]+HP\))|(Health: [\d,]+\/[\d,]+HP\.)/;
-			var healthMessage = message.content.match(healthMessagePattern);
-			if (healthMessage) {
-				var nums = healthMessage[0].match(/([\d,]+)\/([\d,]+)/);
-				player.currentHP = Number(nums[1].replace(/,/g,""));
-				player.maxHP = Number(nums[2].replace(/,/g,""));
-				player.name = message.content.split(`\n`)[1].replace("'s Adventure ]========!", "").replace("!========[ ", "");
-			}
-			const messageArray = message.content.split('\n');
-			var dealtLine = null, tookLine = null;
-			for (let i in messageArray) {
-				if (messageArray[i].indexOf(' dealt ') !== -1) dealtLine = parseInt(i);
-				if (messageArray[i].indexOf(' took' ) !== -1) tookLine = parseInt(i);
-			}
-			if (tookLine === dealtLine + 1) {
-				const petInfosLine = messageArray[tookLine + 1].split(' ');
-				pet.currentHP = parseInt(petInfosLine[petInfosLine.indexOf('has') + 1].split('/')[0].replace(',', ''));
-				pet.maxHP = parseInt(petInfosLine[petInfosLine.indexOf('has') + 1].split('/')[1].replace(',', ''));
-			}
-		}
-	}
-	else {
-		if (message.embeds[0].author != undefined) {
-			if (message.embeds[0].author.name.indexOf("Adventure") != -1) {
-				const char_field = message.embeds[0].fields[1].name == "Critical Hit!" ? message.embeds[0].fields[4] : message.embeds[0].fields[3];
-				player.currentHP = parseInt(char_field.value.split(' ')[1].split('/')[0].replace(',', ''));
-				player.maxHP = parseInt(char_field.value.split(' ')[1].split('/')[1].replace(',', ''));
+const deathimage = require("../../Data/imageurls.json");
 
-				const pet_field = message.embeds[0].fields[1].name == "Critical Hit!" ? message.embeds[0].fields[3] : message.embeds[0].fields[2];
-				pet.currentHP = parseInt(pet_field.value.split(' ')[1].split('/')[0].replace(',', ''));
-				pet.maxHP = parseInt(pet_field.value.split(' ')[1].split('/')[1].replace(',', ''));
+module.exports = (client, message) => {
+  if (message.guild.settings.healthMonitor === "off") return;
+  const senddeath =
+    deathimage.deathimage[Math.random() * deathimage.deathimage.length];
+  const player = {
+    name: null,
+    currentHP: 0,
+    maxHP: 0
+  };
+  const pet = {
+    currentHP: 0,
+    maxHP: 0
+  };
+  if (message.embeds.length === 0) {
+    if (message.content.indexOf("'s Adventure") !== -1) {
+      const healthMessagePattern = /( has [\d,]+\/[\d,]+ HP left\.)|(used .+? and got [\d,]+?HP\. \([\d,]+\/[\d,]+HP\))|(Health: [\d,]+\/[\d,]+HP\.)/;
+      const healthMessage = message.content.match(healthMessagePattern);
+      if (healthMessage) {
+        const nums = healthMessage[0].match(/([\d,]+)\/([\d,]+)/);
+        player.currentHP = Number(nums[1].replace(/,/g, ""));
+        player.maxHP = Number(nums[2].replace(/,/g, ""));
+        player.name = message.content
+          .split(`\n`)[1]
+          .replace("'s Adventure ]======!", "")
+          .replace("!======[ ", "");
+      }
+      const messageArray = message.content.split("\n");
+      let dealtLine = null;
+      let tookLine = null;
+      for (const i in messageArray) {
+        if (messageArray[i].indexOf(" dealt ") !== -1)
+          dealtLine = parseInt(i, 10);
+        if (messageArray[i].indexOf(" took ") !== -1)
+          tookLine = parseInt(i, 10);
+      }
+      if (tookLine === dealtLine + 1) {
+        const petInfosLine = messageArray[tookLine + 1].split(" ");
+        pet.currentHP = parseInt(
+          petInfosLine[petInfosLine.indexOf("has") + 1]
+            .split("/")[0]
+            .replace(",", ""),
+          10
+        );
+        pet.maxHP = parseInt(
+          petInfosLine[petInfosLine.indexOf("has") + 1]
+            .split("/")[1]
+            .replace(",", ""),
+          10
+        );
+      }
+    }
+  } else if (message.embeds[0].author !== undefined) {
+    if (message.embeds[0].author.name.indexOf("Adventure") !== -1) {
+      const charField =
+        message.embeds[0].fields[1].name === "Critical Hit!"
+          ? message.embeds[0].fields[4]
+          : message.embeds[0].fields[3];
+      player.currentHP = parseInt(
+        charField.value
+          .split(" ")[1]
+          .split("/")[0]
+          .replace(",", ""),
+        10
+      );
+      player.maxHP = parseInt(
+        charField.value
+          .split(" ")[1]
+          .split("/")[1]
+          .replace(",", ""),
+        10
+      );
 
-				player.name = char_field.name;
-			}
-		}
-	}
-  
-	const matchedMessage = message.channel.messages.find(msg => msg.author.username === player.name);
-	var user = null;
-	if (matchedMessage !== undefined) {
-		user = matchedMessage.author;
-	} else return;
-	player.healthPercent = Math.round(10 * player.currentHP * 100 / player.maxHP) / 10;
-	pet.healthPercent = Math.round(10 * pet.currentHP * 100 / pet.maxHP) / 10;
-	if (user !== undefined && player.name !== undefined && player.healthPercent !== pet.healthPercent) {
-		const execute = (individual, healthMonitor) => {
-			const playerWarning = () => {
-				const embed = new Discord.MessageEmbed()
-					  .setTitle(`__${user.username} Health Warning!!! - ${player.healthPercent}%__`)
-					.setColor(0xff0000)
-					.setDescription(`**${user.username}** is at __**${player.currentHP}**__ health!!!\n`)
-					.setImage(`${senddeath}`)
-					.setFooter(`You are going to die aren't you?`)
-				message.channel.send(embed).then(msg => msg.delete({ timeout: 60000 }));
-			}, petWarning = () => {
-				const embed = new Discord.MessageEmbed()
-					.setTitle(`__${user.username} PET Health Warning!!! - ${pet.healthPercent}%__`)
-					.setColor(0xff0000)
-					.setDescription(`**${user.username}** your pet is at __**${pet.currentHP}**__ health!!!\n`)
-					.setImage(`${senddeath}`)
-					.setFooter(`OMG YOU ARE GOING TO LET YOUR PET DIE????`)
-			  message.channel.send(embed).then(msg => msg.delete({ timeout: 60000 }));
-			}, general = () => {
-				var embed = new Discord.MessageEmbed()
-					 .setAuthor(user.username, user.avatarURL())
-					 .setFooter(`&uconfig To Change Health Monitor Settings`)
-  
-				if (individual !== 'pet') embed.addField(`__Character Health__ - **${player.healthPercent}%**`,`(${player.currentHP} HP / ${player.maxHP} HP)`, false)
-				if (!isNaN(pet.healthPercent) && individual !== 'character') embed.addField(`__Pet Health__ - **${pet.healthPercent}%**`, `(${pet.currentHP} HP / ${pet.maxHP} HP)`, false);
-				if ((isNaN(pet.healthPercent) || pet.healthPercent === 0) && individual !== 'character') embed.addField(`__Pet Condition__`, `**DEAD** or not able to do damages`);
-				if (embed.fields.length === 0) return;
-				if (player.healthPercent <= 20 || pet.healthPercent <= 20) embed.setColor('RED');
-				else if (player.healthPercent <= 40 || pet.healthPercent <= 40) embed.setColor('ORANGE');
-				else if (player.healthPercent <= 60 || pet.healthPercent <= 60) embed.setColor('GOLD');
-				else if (player.healthPercent <= 100 || pet.healthPercent <= 100) embed.setColor('GREEN');
-				message.channel.send({embed}).then(msg => msg.delete({ timeout: 30000 }));
-			}
+      const petField =
+        message.embeds[0].fields[1].name === "Critical Hit!"
+          ? message.embeds[0].fields[3]
+          : message.embeds[0].fields[2];
+      pet.currentHP = parseInt(
+        petField.value
+          .split(" ")[1]
+          .split("/")[0]
+          .replace(",", ""),
+        10
+      );
+      pet.maxHP = parseInt(
+        petField.value
+          .split(" ")[1]
+          .split("/")[1]
+          .replace(",", ""),
+        10
+      );
 
-			if (['character', "pet"].indexOf(individual) !== -1) {
-				if (individual === 'character' && player.healthPercent < 11 && (player.healthPercent <= healthMonitor || healthMonitor === 'on')) return playerWarning();
-				if (individual === 'pet' && pet.healthPercent < 11 && (pet.healthPercent <= healthMonitor || healthMonitor === 'on')) return petWarning();
-				if (player.healthPercent <= healthMonitor || pet.healthPercent <= healthMonitor || healthMonitor === 'on') return general();
-			} else {
-				if (player.healthPercent < 11 && (player.healthPercent <= healthMonitor || healthMonitor === 'on')) return playerWarning();
-				else if (pet.healthPercent < 11 && (pet.healthPercent <= healthMonitor || healthMonitor === 'on')) return petWarning();
-				else if (player.healthPercent <= healthMonitor || pet.healthPercent <= healthMonitor || healthMonitor === 'on') return general();
-			}
-		}
+      player.name = charField.name;
+    }
+  }
 
-		if (user.settings.healthMonitor === 'off') return;
-		execute(user.settings.individualHealthMonitor, user.settings.healthMonitor);
-	}
-}
+  const matchedMessage = message.channel.messages.find(
+    msg => msg.author.username === player.name
+  );
+  let user = null;
+  if (matchedMessage !== undefined) {
+    user = matchedMessage.author;
+  } else return;
+  player.healthPercent =
+    Math.round((1000 * player.currentHP) / player.maxHP) / 10;
+  pet.healthPercent = Math.round((10 * pet.currentHP * 100) / pet.maxHP) / 10;
+  if (
+    user !== undefined &&
+    player.name !== undefined &&
+    player.healthPercent !== pet.healthPercent
+  ) {
+    if (user.settings.healthMonitor === "off") return;
+    const { healthMonitor, individualHealthMonitor } = user.settings;
+    const playerWarning = () => {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(
+          `__${user.username} Health Warning!!! - ${player.healthPercent}%__`
+        )
+        .setColor(0xff0000)
+        .setDescription(
+          `**${user.username}** is at __**${player.currentHP}**__ health!!!\n`
+        )
+        .setImage(senddeath)
+        .setFooter(`You are going to die aren't you?`);
+      message.channel.send(embed).then(msg => msg.delete({ timeout: 60000 }));
+    };
+    const petWarning = () => {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(
+          `__${user.username} PET Health Warning!!! - ${pet.healthPercent}%__`
+        )
+        .setColor(0xff0000)
+        .setDescription(
+          `**${user.username}** your pet is at __**${
+            pet.currentHP
+          }**__ health!!!\n`
+        )
+        .setImage(senddeath)
+        .setFooter(`OMG YOU ARE GOING TO LET YOUR PET DIE????`);
+      message.channel.send(embed).then(msg => msg.delete({ timeout: 60000 }));
+    };
+    const general = () => {
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(user.username, user.avatarURL())
+        .setFooter(`&uconfig To Change Health Monitor Settings`);
+
+      if (individualHealthMonitor !== "pet")
+        embed.addField(
+          `__Character Health__ - **${player.healthPercent}%**`,
+          `(${player.currentHP} HP / ${player.maxHP} HP)`,
+          false
+        );
+      if (
+        !Number.isNaN(pet.healthPercent) &&
+        individualHealthMonitor !== "character"
+      ) {
+        embed.addField(
+          `__Pet Health__ - **${pet.healthPercent}%**`,
+          `(${pet.currentHP} HP / ${pet.maxHP} HP)`,
+          false
+        );
+      }
+      if (
+        (Number.isNaN(pet.healthPercent) || pet.healthPercent === 0) &&
+        individualHealthMonitor !== "character"
+      ) {
+        embed.addField(
+          `__Pet Condition__`,
+          `**DEAD** or not able to deal damages.`
+        );
+      }
+      if (embed.fields.length === 0) return;
+      if (player.healthPercent <= 20 || pet.healthPercent <= 20)
+        embed.setColor("RED");
+      else if (player.healthPercent <= 40 || pet.healthPercent <= 40)
+        embed.setColor("ORANGE");
+      else if (player.healthPercent <= 60 || pet.healthPercent <= 60)
+        embed.setColor("GOLD");
+      else if (player.healthPercent <= 100 || pet.healthPercent <= 100)
+        embed.setColor("GREEN");
+      message.channel
+        .send({ embed })
+        .then(msg => msg.delete({ timeout: 30000 }));
+    };
+
+    if (["character", "pet"].indexOf(individualHealthMonitor) !== -1) {
+      if (
+        individualHealthMonitor === "character" &&
+        player.healthPercent < 11 &&
+        (player.healthPercent <= healthMonitor || healthMonitor === "on")
+      ) {
+        return playerWarning();
+      }
+      if (
+        individualHealthMonitor === "pet" &&
+        pet.healthPercent < 11 &&
+        (pet.healthPercent <= healthMonitor || healthMonitor === "on")
+      ) {
+        return petWarning();
+      }
+      if (
+        player.healthPercent <= healthMonitor ||
+        pet.healthPercent <= healthMonitor ||
+        healthMonitor === "on"
+      ) {
+        return general();
+      }
+    } else if (
+      player.healthPercent < 11 &&
+      (player.healthPercent <= healthMonitor || healthMonitor === "on")
+    ) {
+      return playerWarning();
+    } else if (
+      pet.healthPercent < 11 &&
+      (pet.healthPercent <= healthMonitor || healthMonitor === "on")
+    ) {
+      return petWarning();
+    } else if (
+      player.healthPercent <= healthMonitor ||
+      pet.healthPercent <= healthMonitor ||
+      healthMonitor === "on"
+    ) {
+      return general();
+    }
+  }
+};
--- structures/Aldebaran/commands/Command.js
+++ structures/Aldebaran/commands/Command.js
@@ -1,63 +1,80 @@
-const CooldownManager = require(`${process.cwd()}/structures/Aldebaran/CooldownManager`);
+const CooldownManager = require(`../CooldownManager`);
 module.exports = class Command {
-    /**
-     * Builds the Command object
-     * @param {string} command Command Name
-     */
-    constructor(client, name, commandFile) {
-        this.client = client;
-        this.commandFile = commandFile;
-        this.cooldownManager = new CooldownManager(this);
-        this.name = name;
-        for (let [key, value] of Object.entries(this.commandFile.infos)) this[key] = value;
+  /**
+   * Builds the Command object
+   * @param {string} command Command Name
+   */
+  constructor(client, name, commandFile) {
+    this.client = client;
+    this.commandFile = commandFile;
+    this.cooldownManager = new CooldownManager(this);
+    this.name = name;
+    for (const element in this.commandFile.infos) {
+      this[element] = this.commandFile.infos[element];
     }
+  }
 
-    log(cooldown, message, args) {
-        console.log(`User ${message.author.id} (${message.author.tag}) | Command ${this.name}${args.length === 0 ? '' : ` | Args - ${args}`} | Cooldown : ${cooldown ? 'GOOD' : `BAD (${cooldown}ms)`}`);
-    }
+  log(cooldown, message, args) {
+    console.log(
+      `User ${message.author.id} (${message.author.tag}) | Command ${
+        this.name
+      }${args.length === 0 ? "" : ` | Args - ${args}`} | Cooldown : ${
+        cooldown ? "GOOD" : `BAD (${cooldown}ms)`
+      }`
+    );
+  }
 
-    check(user) {
-        if (this.permissions !== undefined) {
-            if (this.permissions.bot !== undefined) {
-                if (this.permissions.bot.length > 0) {
-                    if (user.asBotStaff !== null) {
-                        if (user.asBotStaff.acknowledgements !== undefined) {
-                            if (user.asBotStaff.acknowledgements.length > 0) {
-                                if (user.asBotStaff.acknowledgements.indexOf('ADMIN') !== -1) return true;
-                                var pass = false;
-                                for (let acknowledgement of Object.entries(this.permissions.bot)) if (user.asBotStaff.acknowledgements.indexOf(acknowledgement) !== -1) pass = true;
-                                return pass;
-                            } else return false;
-                        } else return false;
-                    } else return false;
-                } else return true;
-            } else return true;
-        } else return true;
-    }
-    
-    /**
-     * Executes the Command
-     * @param {Client} bot Discord Client
-     * @param {Message} message Message Object
-     * @param {string[]} args Command Ags
-     */
-    async execute(bot, message, args) {
-        if (this.nsfw && !message.channel.nsfw) return message.channel.send(`This command only works in a NSFW channel!`);
-        const canPass = this.cooldownManager.canPass(message.author);
-        this.log(canPass, message, args);
-        if (canPass) {
-            if (this.check(message.author)) {
-                try {
-                    await bot.database.commands.create(this.name, args, message);
-                } catch(err) {
-                    await bot.database.commands.create(this.name, "[]", message);
-                }
-                return this.commandFile.run(bot, message, args);
-            } else {
-                return new Error('Insufficient Bot Permissions');
+  check(user) {
+    if (this.permissions !== undefined) {
+      if (this.permissions.bot !== undefined) {
+        if (this.permissions.bot.length > 0) {
+          if (user.asBotStaff !== null) {
+            if (user.asBotStaff.acknowledgements !== undefined) {
+              if (user.asBotStaff.acknowledgements.length > 0) {
+                if (user.asBotStaff.acknowledgements.indexOf("ADMIN") !== -1)
+                  return true;
+                let pass = false;
+                for (const acknowledgement in this.permissions.bot)
+                  if (
+                    user.asBotStaff.acknowledgements.indexOf(
+                      acknowledgement
+                    ) !== -1
+                  )
+                    pass = true;
+                return pass;
+              }
             }
-        } else {
-            return new Error('Exceeded Command Cooldown');
+          }
+        } else return true;
+      } else return true;
+    } else return true;
+    return false;
+  }
+
+  /**
+   * Executes the Command
+   * @param {Client} bot Discord Client
+   * @param {Message} message Message Object
+   * @param {string[]} args Command Ags
+   */
+  async execute(bot, message, args) {
+    if (this.nsfw && !message.channel.nsfw)
+      return message.channel.send(`This command only works in a NSFW channel!`);
+    const canPass = this.cooldownManager.canPass(message.author);
+    this.log(canPass, message, args);
+    if (canPass) {
+      if (this.check(message.author)) {
+        try {
+          await bot.database.commands.create(this.name, args, message);
+        } catch (err) {
+          await bot.database.commands.create(this.name, "[]", message);
         }
+      } else {
+        return new Error("Insufficient Bot Permissions");
+      }
+    } else {
+      return new Error("Exceeded Command Cooldown");
     }
-}
+    return this.commandFile.run(bot, message, args);
+  }
+};
