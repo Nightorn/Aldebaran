22e8ed62ac2805b6837dc234e2c8f3119dd0f6a0
Ciborn <rb.ciborn+dev@gmail.com>
2019-04-19 17:25:09 +0200

    add: math command

---
+++ Commands/utilities/math.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+const mathjs = require("mathjs");
+
+exports.run = (bot, message, args) => {
+  let result;
+  try {
+    result = args.join(" ") === "10 + 9" ? 21 : mathjs.eval(args.join(" "));
+  } catch (err) {
+    result = "The specified math expression is invalid.";
+  }
+  const embed = new MessageEmbed()
+    .setTitle("Math Expression Evaluation")
+    .addField("Result", `\`\`\`${result}\`\`\``)
+    .setColor("#dc3912");
+  message.channel.send({ embed });
+};
+
+exports.infos = {
+  category: "Utilities",
+  description: "Evaluates a math expression",
+  usage: "&math <expression>",
+  example: "&math 10 + 9"
+};
