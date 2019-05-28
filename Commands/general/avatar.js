f14180811c8d03e3567f721cf06623c1018dade7
Ciborn <ciborn@nightorn.com>
2019-05-28 17:08:45 +0200

    fix: &avatar

--- Commands/general/avatar.js
+++ Commands/general/avatar.js
@@ -1,21 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+const userCheck = require("../../functions/action/userCheck");
+
 exports.run = (bot, message, args) => {
-	const { MessageEmbed } = require('discord.js');
-	require(`${process.cwd()}/functions/action/userCheck`)(bot, message, args).then(usrid => {
-		const user = bot.users.fetch(usrid);
-		const embed = new MessageEmbed()
-			.setAuthor(user.username, user.avatarURL())
-			.setTitle(`${user.username}'s Avatar`)
-			.setImage(user.avatarURL({ size: 2048 }));
-		message.channel.send({embed});
-	}).catch(() => {
-        message.reply("The ID of the user you specified is invalid. Please retry by mentionning him or by getting their right ID.");
-	});
-}
+  userCheck(bot, message, args)
+    .then(async usrid => {
+      const user = await bot.users.fetch(usrid);
+      const embed = new MessageEmbed()
+        .setAuthor(user.username, user.avatarURL())
+        .setTitle(`${user.username}'s Avatar`)
+        .setImage(user.avatarURL({ size: 2048 }));
+      message.channel.send({ embed });
+    })
+    .catch(() => {
+      message.reply(
+        "The ID of the user you specified is invalid. Please retry by mentionning him or by getting their right ID."
+      );
+    });
+};
 
 exports.infos = {
-      category: "General",
-      description: "Displays Mentioned Users Avatar Image.",
-      usage: "\`&avatar <usermention>\`",
-      example: "\`&avatar @aldebaran\`"
-  }
-        
+  category: "General",
+  description: "Displays Mentioned Users Avatar Image.",
+  usage: "`&avatar <usermention>`",
+  example: "`&avatar @aldebaran`"
+};
