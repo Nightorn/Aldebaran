44e6ca18dccf8dc717ed945fd7ef14021b8563e2
Ciborn <ciborn@nightorn.com>
2019-05-31 15:11:32 +0200

    add: &panda

--- Commands/image/panda.js
+++ Commands/image/panda.js
@@ -1,24 +1,27 @@
-exports.run = (bot, message, args) => {
-    const request = require(`request`);
-    const { MessageEmbed } = require(`discord.js`);
-    var pandanumber = Math.floor((Math.random() * 52) + 1);
-    request({uri: `https://api.pexels.com/v1/search?query=panda+query&per_page=1&page=${pandanumber}`,headers: {"Authorization":bot.config.pexels_apikey}}, function (err, response, body) {
-        var parsed = JSON.parse(body);
-        if (err) return message.channel.send("There seems to be a prickly problem.");
-        if (data.error) return message.channel.send(`Someone has requested too many pandas recently, the only thing you can do is waiting for your turn!`);
-        var data = parsed.photos[0];
-        const embed = new MessageEmbed()
-            .setTitle(`**__Panda Panda Panda__**`)
-            .setAuthor(message.author.username,message.author.avatarURL())
-            .setColor(0x00AE86)
-            .setImage(data.src.large)
-            .setFooter(`Panda Powered By: ${data.photographer} on Pexels.com`)
-        message.channel.send({embed})
-    })
-}
+const request = require("request");
+const { MessageEmbed } = require("discord.js");
+
+exports.run = (bot, message) => {
+  request(
+    { uri: "https://some-random-api.ml/img/panda" },
+    (err, response, body) => {
+      const { link } = JSON.parse(body);
+      const embed = new MessageEmbed()
+        .setTitle(`**__Panda Panda Panda__**`)
+        .setAuthor(message.author.username, message.author.avatarURL())
+        .setColor(0x00ae86)
+        .setImage(link)
+        .setFooter(
+          `Your panda has been delivered with ${link} via Some Random Api!`
+        );
+      message.channel.send({ embed });
+    }
+  );
+};
+
 exports.infos = {
-    category: "Image",
-    description: "Displays a random panda picture or gif.",
-    usage: "\`&panda\`",
-    example: "\`&panda\`",
-}
+  category: "Image",
+  description: "Displays a random panda picture or gif.",
+  usage: "`&panda`",
+  example: "`&panda`"
+};
