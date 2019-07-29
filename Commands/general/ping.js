d38eed7637e0b144c6f677ce195b0a7b5fcc8e86
Mount2010 <mount2010@nightorn.com>
2019-07-29 14:53:33 +0800

    Create curconv command

--- Commands/general/ping.js
+++ Commands/general/ping.js
@@ -1,48 +1,51 @@
-exports.run = async (bot, message) => {
-    const { MessageEmbed } = require('discord.js');
-    const embed = new MessageEmbed()
-        .addField('WebSocket Heartbeat', `${Math.floor(bot.ping)} ms`, true)
-        .addField(`${bot.user.username} Ping`, `Computing...`, true)
-        .setColor('BLUE');
-    const newMessage = await message.channel.send({embed});
-    const ping = newMessage.createdTimestamp - message.createdTimestamp;
-    const messages = {
-        good: [
-            "This latency looks pretty low!",
-            "Gotta go fast!"
-        ],
-        average: [
-            "Not the best latency ever, but it's alright.",
-            "Hey it's laggy! It's still working though?"
-        ],
-        bad: [
-            "Oops, looks like we are running slow.",
-            "This number is way too high!"
-        ]
+const Command = require("../../structures/commands/Command");
+module.exports = class PingCommand extends Command {
+    constructor (client) {
+        super(client, {
+            name: "ping",
+            description: "Displays the bot's current ping to Discord",
+            category: "General",
+        });
     }
-    var color = 'BLUE', desc = "Hi.";
+    async run (bot, message, args) {
+        const { MessageEmbed } = require('discord.js');
+        const embed = new MessageEmbed()
+            .addField('WebSocket Heartbeat', `${Math.floor(bot.ping)} ms`, true)
+            .addField(`${bot.user.username} Ping`, `Computing...`, true)
+            .setColor('BLUE');
+        const newMessage = await message.channel.send({embed});
+        const ping = newMessage.createdTimestamp - message.createdTimestamp;
+        const messages = {
+            good: [
+                "This latency looks pretty low!",
+                "Gotta go fast!"
+            ],
+            average: [
+                "Not the best latency ever, but it's alright.",
+                "Hey it's laggy! It's still working though?"
+            ],
+            bad: [
+                "Oops, looks like we are running slow.",
+                "This number is way too high!"
+            ]
+        }
+        var color = 'BLUE', desc = "Hi.";
 
-    if (ping <= 500) {
-        color = 'GREEN';
-        desc = messages.good[Math.floor(Math.random() * messages.good.length)];
-    } else if (ping > 1000) {
-        color = 'RED';
-        desc = messages.bad[Math.floor(Math.random() * messages.bad.length)];
-    } else if (ping > 500) {
-        color = 'ORANGE';
-        desc = messages.average[Math.floor(Math.random() * messages.average.length)];
+        if (ping <= 500) {
+            color = 'GREEN';
+            desc = messages.good[Math.floor(Math.random() * messages.good.length)];
+        } else if (ping > 1000) {
+            color = 'RED';
+            desc = messages.bad[Math.floor(Math.random() * messages.bad.length)];
+        } else if (ping > 500) {
+            color = 'ORANGE';
+            desc = messages.average[Math.floor(Math.random() * messages.average.length)];
+        }
+
+        const embedResult = new MessageEmbed()
+            .addField('WebSocket Heartbeat', `${Math.floor(bot.ws.ping)} ms`, true)
+            .addField(`${bot.user.username} Ping`, `${ping} ms`, true)
+            .setColor(color);
+        newMessage.edit(desc, {embed: embedResult});
     }
-
-    const embedResult = new MessageEmbed()
-        .addField('WebSocket Heartbeat', `${Math.floor(bot.ws.ping)} ms`, true)
-        .addField(`${bot.user.username} Ping`, `${ping} ms`, true)
-        .setColor(color);
-    newMessage.edit(desc, {embed: embedResult});
 };
-
-exports.infos = {
-    category: "General",
-    description: "Displays Current Bot Ping",
-    usage: "\`&ping\`",
-    example: "\`&ping\`",
-}
