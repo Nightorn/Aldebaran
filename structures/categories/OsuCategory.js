1c9def31a45ae4b304c73069138e850c935bbb88
Ciborn <ciborn@nightorn.com>
2019-08-30 00:39:27 +0200

    fix: api key check for osu! commands

--- structures/categories/OsuCategory.js
+++ structures/categories/OsuCategory.js
@@ -16,6 +16,6 @@ module.exports.Command = class OsuCategory extends Command {
 
 	registerCheck() {
 		return this.client.config.apikeys["osu!"] !== undefined
-			|| this.client.config.apikeys["osu!"] !== null;
+			&& this.client.config.apikeys["osu!"] !== null;
 	}
 };
