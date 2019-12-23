a68742d3f42897efc54f04cd175777283dc354cc
Ciborn <ciborn@nightorn.com>
2019-12-23 17:44:20 +0100

    add: guide for command aliases

---
+++ Commands/general/commands/guide.js
@@ -0,0 +1,19 @@
+const { Command, Embed } = require("../../../structures/categories/GeneralCategory");
+
+module.exports = class AliasCommandsSubcommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: "guide",
+			description: "Displays help on how to use the customized commands",
+			subcommand: true
+		});
+	}
+
+	run(bot, message) {
+		const cmd = `**${message.guild.prefix}commands `;
+		const embed = new Embed(this)
+			.setAuthor("Customized Commands  |  Guide", bot.user.avatarURL())
+			.setDescription(`These are the subcommands you can use to customize the available commands in your server.\n${cmd}alias** Creates an alias from another command\n${cmd}disable** Disables an existing command or removes an alias\n${cmd}enable** Enables back a command you have previously disabled`);
+		message.channel.send({ embed });
+	}
+};
