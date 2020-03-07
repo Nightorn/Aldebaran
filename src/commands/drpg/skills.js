bf98349e81f8ca35f812888bf6b8d2e5e32bbc2d
Ciborn <ciborn@nightorn.com>
2020-03-07 23:05:07 +0100

    update: finish &skills

--- src/commands/drpg/skills.js
+++ src/commands/drpg/skills.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const request = require("request");
 const { Command } = require("../../groups/DRPGCommand");
+const itemList = require("../../../assets/data/drpg/itemList.json");
 
 module.exports = class SkillsCommand extends Command {
 	constructor(client) {
@@ -14,34 +15,43 @@ module.exports = class SkillsCommand extends Command {
 
 	// eslint-disable-next-line class-methods-use-this
 	run(bot, message, args) {
-		bot.users.fetch(args.user || message.author.id).then(usrid => {
-			request({ uri: `http://api.discorddungeons.me/v3/user/${usrid}`, headers: { Authorization: bot.config.drpg_apikey } }, (err, response, body) => {
-				if (err) return;
-				const { data } = JSON.parse(body);
-				if (data.error !== undefined) message.channel.send("**Error**: No User Profile Found.");
-				else {
-					const maxpoints = Math.floor(data.level * 5);
+		bot.users.fetch(args.user || message.author.id).then(user => {
+			request({ uri: `http://api.discorddungeons.me/v3/user/${user.id}`, headers: { Authorization: bot.config.drpg_apikey } }, (err, response, body) => {
+				if (err) throw err;
+				if (response.statusCode === 404) {
+					message.reply(
+						"it looks like the user you specified has not started his adventure on DiscordRPG yet."
+					);
+				} else if (response.statusCode === 200) {
+					const { data } = JSON.parse(body);
+					const maxpoints = data.level * 5;
 					const skillinfo = data.skills;
-					const currentpoints = data.attributes;
-					const { tool } = data;
-					let axebonus = 0;
-					if (tool.axe === "370") axebonus = 0;
-					else if (tool.axe === "413") axebonus += 1;
-					else if (tool.axe === "415") axebonus += 3;
-					else if (tool.axe === "416") axebonus += 5;
-					else if (tool.axe === "417") axebonus += 8;
-					else axebonus = 0;
+					const attribs = data.attributes;
+
+					const axebonus = itemList[data.tool.axe].itemBoost || 0;
 					const lumbercurrent = Math.floor((
-						((currentpoints.lumberBoost / 25) + 1) + skillinfo.chop.level)
+						((attribs.lumberBoost / 25) + 1) + skillinfo.chop.level)
 					+ axebonus);
 					const lumbermax = Math.floor(
 						(((maxpoints / 25) + 1) + skillinfo.chop.level) + axebonus
 					);
+					const pickbonus = itemList[data.tool.pickaxe].itemBoost || 0;
+					const miningCurrent = 1 + skillinfo.mine.level
+						+ Math.floor(attribs.mineBoost / 40) + pickbonus;
+					const miningMax = 1 + skillinfo.mine.level
+						+ Math.floor(maxpoints / 40);
+					const forageCurrent = 1 + skillinfo.forage.level
+						+ Math.floor(attribs.scavenge / 65);
+					const forageMax = 1 + skillinfo.forage.level
+						+ Math.floor(maxpoints / 65);
 
 					const embed = new MessageEmbed()
 						.setAuthor(`${message.author.username}  |  Skills`, message.author.avatarURL())
 						.setColor(0x00AE86)
-						.addField("Chopping", `**Level ${skillinfo.chop.level}** (${Number.formatNumber(skillinfo.chop.xp)} XP)\nWith your **current** lumber boost skills, you would get **${lumbercurrent} logs**.\nWith the **highest** lumber boost skills, you would get **${lumbermax} logs**.`);
+						.addField("Mining", `**Level ${skillinfo.mine.level}** (${Number.formatNumber(skillinfo.mine.xp)} XP)\nWith your **current** mining boost skills, you would get **${lumbercurrent} ores**.\nWith the **highest** mining boost skills, you would get **${lumbermax} ores**.`)
+						.addField("Chopping", `**Level ${skillinfo.chop.level}** (${Number.formatNumber(skillinfo.chop.xp)} XP)\nWith your **current** lumber boost skills, you would get **${miningCurrent} logs**.\nWith the **highest** lumber boost skills, you would get **${miningMax} logs**.`)
+						.addField("Foraging", `**Level ${skillinfo.forage.level}** (${Number.formatNumber(skillinfo.forage.xp)} XP)\nWith your **current** scavenging skills, you would get **${forageCurrent} items**.\nWith the **highest** scavenging skills, you would get **${forageMax} items**.`)
+						.addField("Fishing", `**Level ${skillinfo.fish.level}** (${Number.formatNumber(skillinfo.fish.xp)} XP)`);
 					message.channel.send(embed);
 				}
 			});
