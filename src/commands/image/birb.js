173c27fb8fc92842794a0e24c435dafbadaa48b0
Ciborn <ciborn@nightorn.com>
2020-04-10 21:30:42 +0200

    fix: prevent &birb from crashing unexpectedly (fixes #39)

--- src/commands/image/birb.js
+++ src/commands/image/birb.js
@@ -8,7 +8,7 @@ module.exports = class BirbCommand extends Command {
 
 	run(bot, message) {
 		request({ uri: "http://random.birb.pw/tweet.json/" }, (err, response, body) => {
-			if (err) return message.channel.send("This seems to be a birb problem");
+			if (err || response.statusCode !== 200) return message.channel.send("This seems to be a birb problem");
 			const embed = new Embed(this)
 				.setTitle("You want some __Birb__?")
 				.setImage(`http://random.birb.pw/img/${JSON.parse(body).file}`)
