5d80dfa51600b7723181dea5ed3a5f518d6bba8a
Ciborn <ciborn@nightorn.com>
2020-04-10 22:09:44 +0200

    fix: ordering of inherited command metadata (fixes #43)

--- src/groups/ActionCommand.js
+++ src/groups/ActionCommand.js
@@ -3,8 +3,8 @@ const { Command, Embed } = require("./Command");
 module.exports.Command = class ActionCommand extends Command {
 	constructor(client, metadata) {
 		super(client, {
-			...metadata,
-			args: { user: { as: "user" } }
+			args: { user: { as: "user" } },
+			...metadata
 		});
 		this.category = "Action";
 		this.color = "AQUA";
