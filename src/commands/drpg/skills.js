0034899fe6a8210ada34ab35290c1dd61f8c8d82
Ciborn <ciborn@nightorn.com>
2020-04-10 21:07:11 +0200

    fix: max mine skill xp gain in &skills (fixes #35)

--- src/commands/drpg/skills.js
+++ src/commands/drpg/skills.js
@@ -49,20 +49,29 @@ module.exports = class SkillsCommand extends Command {
 					const forageMax = 1 + skillinfo.forage.level
 						+ Math.floor(maxpoints / 65);
 
+					const xpCalc = level => [
+						Math.round(Math.sqrt((Math.sqrt(level) * 0.25) * 15 / 2)),
+						Math.round(Math.sqrt((Math.sqrt(level) * 0.25) * 5 / 2))
+					];
 					const xp = level => {
-						const max = Math
-							.round(Math.sqrt((Math.sqrt(level) * 0.25) * 15 / 2));
-						const min = Math
-							.round(Math.sqrt((Math.sqrt(level) * 0.25) * 5 / 2));
+						const [max, min] = xpCalc(level);
 						return `You would get between **${min} and ${max} skill XP**.`;
+					};
+					const mineXp = level => {
+						const [max, min] = xpCalc(level);
+						const minMin = min + Math.floor(attribs.mineBoost / 125);
+						const minMax = max + Math.floor(attribs.mineBoost / 125);
+						const maxMin = min + Math.floor(maxpoints / 125);
+						const maxMax = max + Math.floor(maxpoints / 125);
+						return `With your current mining boosts skills, you would get between **${minMin} and ${minMax} skill XP**.\nWith the highest mining boosts skills, you would get between **${maxMin} and ${maxMax} skill XP**.`;
 					};
 					const embed = new MessageEmbed()
 						.setAuthor(`${user.username}  |  Skills`, user.avatarURL())
 						.setColor(0x00AE86)
-						.addField("Mining", `**Level ${skillinfo.mine.level}** (${Number.formatNumber(skillinfo.mine.xp)} XP)\nWith your **current** mining boost skills, you would get **${miningCurrent} ores or essences**.\nWith the **highest** mining boost skills, you would get **${miningMax} ores**, or between **${essenceMax} and ${miningMax} essences**.\n${xp(skillinfo.mine.level)}`)
-						.addField("Chopping", `**Level ${skillinfo.chop.level}** (${Number.formatNumber(skillinfo.chop.xp)} XP)\nWith your **current** lumber boost skills, you would get **${lumbercurrent} logs**.\nWith the **highest** lumber boost skills, you would get **${lumbermax} logs**.\n${xp(skillinfo.mine.level)}`)
-						.addField("Foraging", `**Level ${skillinfo.forage.level}** (${Number.formatNumber(skillinfo.forage.xp)} XP)\nWith your **current** scavenging skills, you would get **${forageCurrent} items**.\nWith the **highest** scavenging skills, you would get **${forageMax} items**.\n${xp(skillinfo.mine.level)}`)
-						.addField("Fishing", `**Level ${skillinfo.fish.level}** (${Number.formatNumber(skillinfo.fish.xp)} XP)\n${xp(skillinfo.mine.level)}`);
+						.addField("Mining", `**Level ${skillinfo.mine.level}** (${Number.formatNumber(skillinfo.mine.xp)} XP)\nWith your **current** mining boost skills, you would get **${miningCurrent} ores or essences**.\nWith the **highest** mining boost skills, you would get **${miningMax} ores**, or between **${essenceMax} and ${miningMax} essences**.\n${mineXp(skillinfo.mine.level)}`)
+						.addField("Chopping", `**Level ${skillinfo.chop.level}** (${Number.formatNumber(skillinfo.chop.xp)} XP)\nWith your **current** lumber boost skills, you would get **${lumbercurrent} logs**.\nWith the **highest** lumber boost skills, you would get **${lumbermax} logs**.\n${xp(skillinfo.chop.level)}`)
+						.addField("Foraging", `**Level ${skillinfo.forage.level}** (${Number.formatNumber(skillinfo.forage.xp)} XP)\nWith your **current** scavenging skills, you would get **${forageCurrent} items**.\nWith the **highest** scavenging skills, you would get **${forageMax} items**.\n${xp(skillinfo.forage.level)}`)
+						.addField("Fishing", `**Level ${skillinfo.fish.level}** (${Number.formatNumber(skillinfo.fish.xp)} XP)\n${xp(skillinfo.fish.level)}`);
 					message.channel.send(embed);
 				}
 			});
