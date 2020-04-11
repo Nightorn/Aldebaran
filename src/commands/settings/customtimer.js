5b84982193e93e4fa315e409770fbf6232d65b43
Ciborn <ciborn@nightorn.com>
2020-04-12 00:13:18 +0200

    fix: &customtimer not working properly (fixes #46)

--- src/commands/settings/customtimer.js
+++ src/commands/settings/customtimer.js
@@ -6,6 +6,7 @@ module.exports = class CustomTimerCommand extends Command {
 	constructor(client) {
 		super(client, {
 			description: "Manages your custom timers",
+			help: "**`&customtimer create`** - Creates a custom timer\n**`&customtimer list`** - Lists your active custom timers\n**`&customtimer delete`** - Deletes the specified custom timer\nThis command allows you to create custom timers. Custom timers are a way for you to be reminded of something every x period of time. For now, this reminder is repeated 10 times; then, it automatically self-destroys. To create a custom timer that reminds you to pet your cat every 5 minutes, use `&customtimer create 5m pet my cat`. If you want to be reminded in the channel in which you are creating the custom timer, add `-c` at the end of the command. If you want to delete the custom timer, use `&customtimer list`, which will give you the list of your active custom timers, look at the ID of the custom timer you want to delete and use `&customtimer delete 60`, if the ID of your custom timer is 60.",
 			usage: "Action",
 			example: "create",
 			aliases: ["ct"]
@@ -25,16 +26,18 @@ module.exports = class CustomTimerCommand extends Command {
 					const channelId = args.includes("-c") ? message.channel.id : null;
 					if (channelId !== null) args.splice(args.indexOf("-c"), 1);
 					if (timer !== null) {
-						timer = timer.reduce((time, str) => time + Number(str.match(/(\d+)\s*([dhms])/i)[1]) * times[RegExp.$2], 0);
+						timer = timer.reduce((time, str) => time + Number(str.match(/(\d+)\s*([dhms])/i)[1]) * times[RegExp.$2], 0); // Parse the interval
 						if (timer < 10000) message.channel.error("WRONG_USAGE", "You cannot set a custom timer with an interval lower than 10 seconds.");
-						// eslint-disable-next-line no-new
-						new CustomTimer(bot, {
+						CustomTimer.create(bot, { // Start the custom timer creation process
 							userId: message.author.id,
 							timer,
 							channelId,
 							content: args.join(" ")
+						}).then(() => {
+							message.channel.send("Your custom timer has been set!");
+						}).catch(err => {
+							message.channel.error("UNEXPECTED_BEHAVIOR", `An unexpected error occured and your custom timer could not be created.\n> ${err.message}`);
 						});
-						message.channel.send("Your custom timer has been set!");
 					} else message.reply("the timer timeout is invalid, it needs to be in the following format: `1337d`, d meaning \"day\", being replaced by s for \"seconds\", m for \"minutes\" and h for \"hours\".");
 				} else message.reply("you need to specify the timer timeout, its content and if the timer should be shown in the current channel instead of in your DMs (-c)");
 			} else if (args[0].toLowerCase() === "delete") {
--- src/structures/aldebaran/CustomTimer.js
+++ src/structures/aldebaran/CustomTimer.js
@@ -1,5 +1,5 @@
 module.exports = class CustomTimer {
-	constructor(client, data) {
+	constructor(client, data, first = false) {
 		this.id = data.id;
 		this.client = client;
 		this.userId = data.userId;
@@ -8,35 +8,24 @@ module.exports = class CustomTimer {
 		this.trigger = data.trigger || null;
 		this.timer = data.timer;
 		this.occurences = data.occurences || 10;
-		this.remind = data.remind;
-		if (this.id === undefined) {
-			if (this.userId === undefined
-				|| this.content === null
-				|| this.timer === null
-			) return new TypeError("INVALID_DATA");
-			this.remind = Date.now();
-			this.create(
-				this.userId, this.content, this.channelId, this.timer,
-				this.trigger, this.occurences
-			);
-			if (this.timer < 2147483648) this.interval();
-		} else if (this.trigger === null && this.timer < 2147483648) {
+		this.remind = data.remind || Date.now();
+		if (this.trigger === null && this.timer < 2147483648) {
 			this.remind = data.remind === null ? this.remind : data.remind;
-			if (this.remind < Date.now()) {
+			if (this.remind < Date.now() && !first) {
 				this.send(true);
 				this.update(true);
-			} else this.timeout();
+			} else this.timeout(first ? this.timer : 0);
 		}
 	}
 
-	timeout() {
+	timeout(delay = 0) {
 		this.timerTimeout = setTimeout(() => {
 			this.occurences--;
 			this.send();
 			if (this.occurences === 0) this.delete();
 			this.update();
 			this.interval();
-		}, this.remind - Date.now());
+		}, this.remind + delay - Date.now());
 	}
 
 	interval() {
@@ -48,19 +37,30 @@ module.exports = class CustomTimer {
 		}, this.timer);
 	}
 
-	async create(userId, content, channelId, timer, trigger, occurences) {
-		if (trigger === null) {
-			const result = await this.client.database
-				.query(`INSERT INTO timers (userId, content, channelId, timer, occurences, remind) VALUES ("${userId}", "${content}", "${channelId}", ${timer}, ${occurences}, ${Date.now()})`);
-			this.id = result.insertId;
-			this.client.customTimers.set(this.id, this);
-			return result;
-		}
-		const result = this.client.database
-			.query(`INSERT INTO timers (userId, content, channelId, trigger, timer) VALUES ("${userId}", "${content}", "${channelId}", ${trigger}, "${timer}")`);
-		this.id = result.insertId;
-		this.client.customTimers.set(this.id, this);
-		return result;
+	static async create(client, data) {
+		return new Promise((resolve, reject) => {
+			const { userId, content, channelId, trigger, timer } = data;
+			const occurences = data.occurences || 10;
+			const remind = Date.now();
+			if (userId && content && timer && trigger === undefined) { // When the custom timer is interval-based
+				const query = channelId !== null
+					? `INSERT INTO timers (userId, content, channelId, timer, occurences, remind) VALUES ("${userId}", "${content}", "${channelId}", ${timer}, ${occurences}, ${remind})`
+					: `INSERT INTO timers (userId, content, timer, occurences, remind) VALUES ("${userId}", "${content}", ${timer}, ${occurences}, ${remind})`;
+				client.database.query(query).then(result => {
+					const id = result.insertId;
+					const customTimer = new CustomTimer(client,
+						{ ...data, remind, id }, true);
+					client.customTimers.set(id, customTimer); // Adds the custom timer to the registry
+					resolve(customTimer);
+				}).catch(reject);
+			/* } else if (userId && content && timer && trigger) { // When the custom timer is trigger-based
+				client.database.query(`INSERT INTO timers (userId, content, channelId, trigger, timer) VALUES ("${userId}", "${content}", "${channelId}", ${trigger}, "${timer}")`).then(result => {
+					const customTimer = new CustomTimer(client, data);
+					client.customTimers.set(result.insertId, customTimer);
+					resolve(customTimer);
+				}).catch(reject); */
+			} else reject(new TypeError("INVALID_DATA"));
+		});
 	}
 
 	async update(fixOffset = false) {
@@ -76,7 +76,11 @@ module.exports = class CustomTimer {
 		const user = await this.client.users.fetch(this.userId);
 		if (this.channelId === null) {
 			user.send(`Here is your reminder${this.content !== null ? ` for **${this.content}**` : ""}!${late ? "\n*This reminder is late due to the bot being down. Sorry for the inconvenience.*" : ""}`).catch(this.delete);
-		} else this.client.channels.cache.get(this.channelId).send(`<@${this.userId}>, here is your reminder${this.content !== null ? ` for **${this.content}**` : ""}!${late ? "\n*This reminder is late due to the bot being down. Sorry for the inconvenience.*" : ""}`).catch(this.delete);
+		} else if (this.client.channels.cache.get(this.channelId)) {
+			this.client.channels.cache.get(this.channelId)
+				.send(`<@${this.userId}>, here is your reminder${this.content !== null ? ` for **${this.content}**` : ""}!${late ? "\n*This reminder is late due to the bot being down. Sorry for the inconvenience.*" : ""}`)
+				.catch(this.delete);
+		}
 	}
 
 	async delete() {
