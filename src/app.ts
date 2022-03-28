8a0c2daa5a75caa6989f86fde03aa3fd89a9b150
Robin Bourachot <me@ciborn.dev>
2022-03-28 09:41:59 +0200

    fix/dev: add type-checking back (for real)

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import api from "./api/api.js";
 const manager = new ShardingManager("./src/bot.ts", {
 	token: process.env.TOKEN,
 	shardArgs: process.argv.slice(2, 4),
-	execArgv: ["--loader", process.argv.includes("dev") ? "ts-node/esm/transpile-only" : "ts-node/esm"]
+	execArgv: ["--loader", process.argv.includes("dev") ? "ts-node/esm" : "ts-node/esm/transpile-only"]
 });
 api(manager);
 manager.on("shardCreate", (shard: Shard) => console.log(`! Launched shard ${shard.id}`));
