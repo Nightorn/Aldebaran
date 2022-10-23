5ed208b6c643a36fdb605d0eb05384a103659555
Robin Bourachot <me@ciborn.dev>
2022-10-23 11:55:17 +0200

    refactor: /trap and bug fixes

--- src/commands/drpg/trap.ts
+++ src/commands/drpg/trap.ts
@@ -5,6 +5,15 @@ import { drpgItems, drpgLocationdb } from "../../utils/Constants.js";
 import { User, Trap } from "../../interfaces/DiscordRPG.js";
 import MessageContext from "../../structures/contexts/MessageContext.js";
 
+const f = (number: number) => String(number).length === 1 ? `0${number}` : number;
+
+function getDate(time: number, md?: boolean) {
+	const date = new Date(time);
+	if (md)
+		return `**${f(date.getMonth() + 1)}/${f(date.getDate())}/${f(date.getFullYear())}** at **${f(date.getHours())}:${f(date.getMinutes())}** UTC`;
+	return `${f(date.getMonth() + 1)}/${f(date.getDate())}/${f(date.getFullYear())} at ${f(date.getHours())}:${f(date.getMinutes())} UTC`;
+}
+
 export default class TrapCommand extends Command {
 	constructor() {
 		super({
@@ -19,7 +28,8 @@ export default class TrapCommand extends Command {
 					flag: { short: "m", long: "max" },
 					optional: true
 				}
-			}
+			},
+			platforms: ["DISCORD", "DISCORD_SLASH"]
 		});
 	}
 
@@ -39,71 +49,72 @@ export default class TrapCommand extends Command {
 		}, async (err, response, body) => {
 			if (err) throw err;
 			if (response.statusCode === 404) {
-				ctx.reply("it looks like the user you specified has not started his adventure on DiscordRPG yet.");
-			} else if (response.statusCode === 200) {
-				const target = await ctx.fetchUser(userid);
-				const user = JSON.parse(body).data as User;
-				let luck = isMax ? user.level * 5 : user.attributes.salvaging;
-				if (luck === 0) luck = 1;
-				const data = user.location.traps;
-				const f = (number: number) => String(number).length === 1 ? `0${number}` : number;
-				const getDate = (time: number, md?: boolean) => {
-					const date = new Date(time);
-					if (md)
-						return `**${f(date.getMonth() + 1)}/${f(date.getDate())}/${f(date.getFullYear())}** at **${f(date.getHours())}:${f(date.getMinutes())}** UTC`;
-					return `${f(date.getMonth() + 1)}/${f(date.getDate())}/${f(date.getFullYear())} at ${f(date.getHours())}:${f(date.getMinutes())} UTC`;
-				};
+				return ctx.reply("it looks like the user you specified has not started his adventure on DiscordRPG yet.");
+			} else if (response.statusCode !== 200) {
+				return ctx.reply("the DiscordRPG API seems down, please retry later.");
+			}
+			
+			const target = await ctx.fetchUser(userid);
+			const user = JSON.parse(body).data as User;
+			let luck = isMax ? user.level * 5 : user.attributes.salvaging;
+			if (luck === 0) luck = 1;
+			const data = user.location.traps;
 
-				if (trapId !== null) {
-					if (data[trapId]) {
-						const trap = data[trapId];
-						const elapsedTime = (Date.now() - trap.time) / 1000;
-						const scope = { luck, passed: elapsedTime };
-						const pronoun = ctx.author.id === userid ? "You" : "They";
-						let items = "";
-						(drpgItems[trap.id].trap as Trap).loot.forEach(item => {
-							const min = evaluate(item.amount.min, scope);
-							const max = evaluate(item.amount.max, scope);
-							const itemName = drpgItems[item.id].name;
-							if (elapsedTime > item.mintime) {
-								if (min === max)
-									items += `- **${min} ${itemName}**\n`;
-								else
-									items += `- Between **${min}** and **${max} ${itemName}**\n`;
-							}
-						});
-						const embed = this.createEmbed()
-							.setAuthor({
-								name: `${target.username}  |  Trap information  |  ${drpgItems[trap.id].name} @ ${drpgLocationdb[trapId]}`,
-								iconURL: target.avatarURL
-							})
-							.setDescription(`${pronoun} will receive the following items (with ${luck} point(s) in salvaging)\n${items}`)
-							.setFooter({
-								text: `${pronoun} have set this trap the ${getDate(trap.time)}. You can use "--max" to get results with the max amount of points in salvaging you can have at your current level.`
-							});
-						ctx.reply(embed);
-					} else {
-						ctx.error(
-							"NOT_FOUND",
-							`You have specified a location where there is no trap. Make sure you are checking the right location by using \`${ctx.prefix}trap\`.`
-						);
-					}
-				} else {
-					let traps = "";
-					for (const [location, trap] of Object.entries(data)) {
-						if (trap.id !== "")
-							traps += `\`[${location}]\` **${drpgItems[trap.id].name}** @ **${drpgLocationdb[location]}** - ${getDate(trap.time, true)}\n`;
-					}
-					const embed = this.createEmbed()
-						.setAuthor({
-							name: `${target.username}  |  Trap information`,
-							iconURL: target.avatarURL
-						})
-						.setDescription(`${ctx.author.id === userid ? "You have" : `**${target.username}** has`} **${traps.match(/\n/g)?.length} traps** set. Please tell us which one you want to view the information of. Use \`${ctx.prefix}trap 4\` for example.\n${traps}`);
-					ctx.reply(embed);
+			if (!data) {
+				return ctx.error(
+					"UNEXPECTED_BEHAVIOR",
+					"Something is wrong with your data, you most likely have never set a trap before. Please do so before using this command again."
+				);
+			} else if (trapId) {
+				if (!data[trapId]) {
+					return ctx.error(
+						"NOT_FOUND",
+						`You have specified a location where there is no trap. Make sure you are checking the right location by using \`${ctx.prefix}trap\`.`
+					);
 				}
+
+				const trap = data[trapId];
+				const elapsedTime = (Date.now() - trap.time) / 1000;
+				const scope = { luck, passed: elapsedTime };
+				const pronoun = ctx.author.id === userid ? "You" : "They";
+				let items = "";
+
+				(drpgItems[trap.id].trap as Trap).loot.forEach(item => {
+					const min = evaluate(item.amount.min, scope);
+					const max = evaluate(item.amount.max, scope);
+					const itemName = drpgItems[item.id].name;
+					if (elapsedTime > item.mintime) {
+						if (min === max)
+							items += `- **${min} ${itemName}**\n`;
+						else
+							items += `- Between **${min}** and **${max} ${itemName}**\n`;
+					}
+				});
+
+				const embed = this.createEmbed()
+					.setAuthor({
+						name: `${target.username}  |  Trap information  |  ${drpgItems[trap.id].name} @ ${drpgLocationdb[trapId]}`,
+						iconURL: target.avatarURL
+					})
+					.setDescription(`${pronoun} will receive the following items (with ${luck} point(s) in salvaging)\n${items}`)
+					.setFooter({
+						text: `${pronoun} have set this trap the ${getDate(trap.time)}. You can use "--max" to get results with the max amount of points in salvaging you can have at your current level.`
+					});
+				ctx.reply(embed);
 			} else {
-				ctx.reply("the DiscordRPG API seems down, please retry later.");
+				let traps = "";
+				for (const [location, trap] of Object.entries(data)) {
+					if (trap.id !== "")
+						traps += `\`[${location}]\` **${drpgItems[trap.id].name}** @ **${drpgLocationdb[location]}** - ${getDate(trap.time, true)}\n`;
+				}
+
+				const embed = this.createEmbed()
+					.setAuthor({
+						name: `${target.username}  |  Trap information`,
+						iconURL: target.avatarURL
+					})
+					.setDescription(`${ctx.author.id === userid ? "You have" : `**${target.username}** has`} **${traps.match(/\n/g)?.length} traps** set. Please tell us which one you want to view the information of. Use \`${ctx.prefix}trap 4\` for example.\n${traps}`);
+				ctx.reply(embed);
 			}
 		});
 	}
